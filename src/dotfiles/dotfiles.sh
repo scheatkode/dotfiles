454764a16b4a6d0e8a6c852f172cc6e9a1c04814
scheatkode <scheatkode@gmail.com>
2021-02-22 00:24:40 +0100

    :poop: chore(cleanup): Cleanup of dirty worktree

---
+++ src/dotfiles/dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Header
+
+# This  script   assumes  a   POSIX  shell  is   present  for   portability  and
+# reproduceability. Any global shellcheck directives are put here as well.
+
+
+# [[file:~/projects/dotfiles/src/dotfiles/dotfiles.org::*Header][Header:1]]
+# shellcheck shell=sh
+# Header:1 ends here
+
+# Colors
+
+# Colors are defined here for better readability of log output.
+
+
+# [[file:~/projects/dotfiles/src/dotfiles/dotfiles.org::*Colors][Colors:1]]
+RED='\e[31m'
+  GREEN='\e[32m'
+ YELLOW='\e[33m'
+   BLUE='\e[34m'
+MAGENTA='\e[35m'
+   CYAN='\e[36m'
+ NORMAL='\e[0m'
+# Colors:1 ends here
