8c528c68faffa0f0ddd5b18ad9f15dd9824770dd
scheatkode <scheatkode@gmail.com>
2021-04-05 00:07:26 +0100

    :recycle: refactor(neovim): Refactor telescope configuration

--- src/neovim/lua/plugins/treesitter.lua
+++ src/neovim/lua/plugins/treesitter.lua
@@ -1,19 +1,50 @@
-require('nvim-treesitter.configs').setup {
-    ensure_installed = 'maintained',
-    highlight = {
-        enable = true,
-    },
-    lsp_interop = {
-        enable = true,
-    },
-    indent = {
-        enable = true,
-    },
+-- treesitter-specific configuration
+-- ensure plugins are loaded
 
-    -- playground specific
-    playground = {
-        enable          = true,
-        updatetime      = 25,    -- debounced time for highlighting nodes
-        persist_queries = false, -- persistence across vim sessions
-    },
+vim.cmd [[packadd nvim-treesitter]]
+vim.cmd [[packadd nvim-treesitter-textobjects]]
+
+-- configure treesitter
+
+require('nvim-treesitter.configs').setup {
+   ensure_installed = 'maintained',
+
+   autotag               = { enable = true },
+   context_commentstring = { enable = true },
+   highlight             = { enable = true },
+   indent                = { enable = true },
+   lsp_interop           = { enable = true },
+
+   textobjects = {
+      lsp_interop = {
+         enable = true,
+      },
+      select = {
+         enable  = true,
+         keymaps = {
+            ['ab'] = '@block.outer',
+            ['ib'] = '@block.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+         },
+      },
+      swap = {
+         enable    = true,
+         swap_next = {
+            ['<leader>cpa'] = '@parameter.inner',
+         },
+         swap_previous = {
+            ['<leader>cpA'] = '@parameter.inner',
+         },
+      },
+   },
+
+   -- playground specific
+   playground = {
+      enable          = true,
+      updatetime      = 25,    -- debounced time for highlighting nodes
+      persist_queries = false, -- persistence across vim sessions
+   },
 }
