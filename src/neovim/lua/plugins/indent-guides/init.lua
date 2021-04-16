dc3610b0939401fa819f850838608ec497801c3f
scheatkode <scheatkode@gmail.com>
2021-04-16 21:41:22 +0100

    :lipstick: fix(neovim): Disable trailing indent guide

--- src/neovim/lua/plugins/indent-guides/init.lua
+++ src/neovim/lua/plugins/indent-guides/init.lua
@@ -1,8 +1,9 @@
 vim.g.indent_blankline_indent_level         = 5
 vim.g.indent_blankline_char                 = '│'
 -- vim.g.indent_blankline_char_list            = {'┊', '│'}
-vim.g.indent_blankline_use_treesitter       = true
-vim.g.indent_blankline_show_current_context = true -- requires treesitter
+vim.g.indent_blankline_use_treesitter                 = true
+vim.g.indent_blankline_show_current_context           = true -- requires treesitter
+vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 vim.g.indentLine_bufTypeExclude  = { 'terminal', }
 vim.g.indentLine_fileTypeExclude = {
@@ -11,15 +12,15 @@ vim.g.indentLine_fileTypeExclude = {
 }
 
 vim.g.indent_blankline_context_patterns = {
-   'class',
-   'function',
-   'method',
-   'if',
-   'while',
-   'for',
-   'object',
-   'table',
-   'block',
    'arguments',
    'array',
+   'block',
+   'class',
+   'for',
+   'function',
+   'if',
+   'method',
+   'object',
+   'table',
+   'while',
 }
