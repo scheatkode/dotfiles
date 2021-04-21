4c2e6220f8a5bd80a3a6ac5869d5470e597808b1
scheatkode <scheatkode@gmail.com>
2021-04-21 12:52:48 +0100

    :sparkles: feat(neovim): Add glyphs

--- src/neovim/lua/meta/icon/glyph.lua
+++ src/neovim/lua/meta/icon/glyph.lua
@@ -1,10 +1,13 @@
 return {
          large_circle = '\u{2b24}',
+         small_circle = '\u{25cf}',
    minus_square_empty = '\u{f147}',
     minus_square_full = '\u{f146}',
     plus_square_empty = '\u{f196}',
      plus_square_full = '\u{f0fe}',
                flickr = '\u{f16e}',
+        up_down_arrow = '\u{2b0d}',
+     left_right_arrow = '\u{2b0c}',
 }
 
 -- Local Variables:
