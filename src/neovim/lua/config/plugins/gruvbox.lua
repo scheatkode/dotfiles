e43de0a746a7dc2ca15eb1f93428c83e29af98e4
scheatkode <scheatkode@gmail.com>
2021-06-02 23:49:26 +0100

    :lipstick: fix(neovim): Fix colorscheme for consistence

--- src/neovim/lua/config/plugins/gruvbox.lua
+++ src/neovim/lua/config/plugins/gruvbox.lua
@@ -2,21 +2,22 @@ vim.o.background                 = 'dark'
 -- e.g.gruvbox_contrast_dark   = 'hard'
 vim.g.gruvbox_bold               = true
 vim.g.gruvbox_italic             = true
-vim.g.gruvbox_underline          = true
+-- vim.g.gruvbox_underline          = true
 vim.g.gruvbox_undercurl          = true
 vim.g.gruvbox_italicize_comments = true
 vim.g.gruvbox_italicize_strings  = true
 
-vim.cmd([[augroup ColorSchemeOverrides]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxGreenSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxOrangeSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxBlueSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxAquaSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxRedSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight GruvboxYellowSign ctermbg=NONE guibg=NONE]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight clear SignColumn]])
-vim.cmd([[autocmd ColorScheme gruvbox highlight clear Statusline]])
-vim.cmd([[augroup END]])
+vim.cmd([[augroup     ColorSchemeOverrides]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight Normal            ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxGreenSign  ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxOrangeSign ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxBlueSign   ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxAquaSign   ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxRedSign    ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight GruvboxYellowSign ctermbg=NONE guibg=NONE]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight clear             SignColumn]])
+vim.cmd([[autocmd     ColorScheme gruvbox highlight clear             Statusline]])
+vim.cmd([[augroup     END]])
 vim.cmd([[colorscheme gruvbox]])
 
 -- Local Variables:
