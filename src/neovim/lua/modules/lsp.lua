dd684eea68c53370593a995f0532c1227fcf22e9
scheatkode <scheatkode@gmail.com>
2021-06-02 23:59:38 +0100

    :bug: fix(neovim): Normalize LSP keymaps for whichkey

--- src/neovim/lua/modules/lsp.lua
+++ src/neovim/lua/modules/lsp.lua
@@ -21,12 +21,12 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<leader>cwa', '<cmd>lua     vim.lsp.buf.add_workspace_folder()<CR>',                       opts)
     buf_set_keymap('n', '<leader>cwr', '<cmd>lua     vim.lsp.buf.remove_workspace_folder()<CR>',                    opts)
     buf_set_keymap('n', '<leader>cwl', '<cmd>lua     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<leader>D',   '<cmd>lua     vim.lsp.buf.type_definition()<CR>',                            opts)
+    buf_set_keymap('n', '<leader>cD',   '<cmd>lua     vim.lsp.buf.type_definition()<CR>',                            opts)
     buf_set_keymap('n', '<leader>cr',  '<cmd>lua     require("lspsaga.rename").rename()<CR>',                       opts)
-    buf_set_keymap('n', '<leader>cl',  '<cmd>Lspsaga show_line_diagnostics<CR>',                                    opts)
+    buf_set_keymap('n', '<leader>cll',  '<cmd>Lspsaga show_line_diagnostics<CR>',                                    opts)
     buf_set_keymap('n', '[d',          '<cmd>lua     require("lspsaga.diagnostic").lsp_jump_diagnostic_prev()<CR>', opts)
     buf_set_keymap('n', ']d',          '<cmd>lua     require("lspsaga.diagnostic").lsp_jump_diagnostic_next()<CR>', opts)
-    buf_set_keymap('n', '<leader>q',   '<cmd>lua     vim.lsp.diagnostic.set_loclist()<CR>',                         opts)
+    buf_set_keymap('n', '<leader>cq',   '<cmd>lua     vim.lsp.diagnostic.set_loclist()<CR>',                         opts)
     buf_set_keymap('n', '<leader>ca',  '<cmd>Lspsaga code_action<CR>',                                              opts)
     buf_set_keymap('x', '<leader>ca',  '<cmd>Lspsaga range_code_action<CR>',                                        opts)
 
@@ -178,6 +178,10 @@ lspconfig['jsonls'].setup {
 --    filetypes = { 'php' },
 --    root_dir  = lspconfig.util.root_pattern('composer.json', '.git')
 -- }
+
+lspconfig['ccls'].setup {
+   on_attach = on_attach
+}
 
 -- Local Variables:
 -- tab-width: 3
