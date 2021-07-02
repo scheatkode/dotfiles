3fe5e1d5c9a96dd8464ced5f963a379c9c15ee6d
scheatkode <scheatkode@gmail.com>
2021-07-02 09:58:41 +0100

    style(neovim): Minor fix to indentation

--- src/neovim/lua/config/plugins/whichkey.lua
+++ src/neovim/lua/config/plugins/whichkey.lua
@@ -34,7 +34,7 @@ whichkey.setup({
    },
 
    window = {
-      border   = "single",         -- none, single, double, shadow
+      border   = "single",       -- none, single, double, shadow
       position = "bottom",       -- bottom, top
       margin   = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
       padding  = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
