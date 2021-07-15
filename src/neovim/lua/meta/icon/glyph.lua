53d75609243b14aa1c87ce989cc011efadf5657e
scheatkode <scheatkode@gmail.com>
2021-07-15 21:09:00 +0100

    fix(neovim): Fix powerline glyphs for stable Neovim

--- src/neovim/lua/meta/icon/glyph.lua
+++ src/neovim/lua/meta/icon/glyph.lua
@@ -1,17 +1,17 @@
 return {
-         large_circle = '\u{2b24}',
-         small_circle = '\u{25cf}',
-   minus_square_empty = '\u{f147}',
-    minus_square_full = '\u{f146}',
-    plus_square_empty = '\u{f196}',
-     plus_square_full = '\u{f0fe}',
-               flickr = '\u{f16e}',
-        up_down_arrow = '\u{2b0d}',
-     left_right_arrow = '\u{2b0c}',
+         large_circle = '⬤',
+         small_circle = '●',
+   minus_square_empty = '',
+    minus_square_full = '',
+    plus_square_empty = '',
+     plus_square_full = '',
+               flickr = '',
+        up_down_arrow = '⬍',
+     left_right_arrow = '⬌',
 }
 
 -- Local Variables:
 -- tab-width: 3
 -- mode: lua
 -- End:
--- vim: set sw=3 ts=3 sts=3 et tw=80
+-- vim: set ft=lua sw=3 ts=3 sts=3 et tw=78:
