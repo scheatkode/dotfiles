8a6cf2761c2a2529cc206e7996f97851b0bab61d
scheatkode <scheatkode@gmail.com>
2021-09-14 22:51:58 +0100

    :zap: refactor(script): Refactor mouse with keyboard script

--- scripts/mouse-with-keyboard.sh
+++ scripts/mouse-with-keyboard.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-if                                          \
-      [ x"${DISPLAY}" != x"" ]              \
-   && command -v setxkbmap > /dev/null 2>&1 \
-; then # X server should be working
-   setxkbmap -option 'keypad:pointerkeys'
-   echo 'All good, press <S-NumPad> to activate.'
-else
-   echo 'Something went wrong, check your X server.'
+if [ -z "${DISPLAY+x}" ] ; then
+   echo "Something's wrong, check your X server."
+   return 1
 fi
+
+# -- X server should be working
+if ! command -v setxkbmap > /dev/null 2>&1 ; then
+   echo "Something's wrong, I can't seem to find the 'setxkbmap' command."
+   return 1
+fi
+
+# -- `setxkbmap` command is available
+setxkbmap -option 'keypad:pointerkeys' \
+   && echo 'All good, press <S-NumLock> to activate.'
+
