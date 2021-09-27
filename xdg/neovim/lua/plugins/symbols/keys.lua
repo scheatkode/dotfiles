ed7b2ef6ae0a484d50a54f13d096e3751ff9a010
scheatkode <scheatkode@gmail.com>
2021-09-27 18:03:56 +0100

    :construction: wip(neovim): Refactor symbols outline plugin configuration

---
+++ xdg/neovim/lua/plugins/symbols/keys.lua
@@ -0,0 +1,6 @@
+return require('util').register_keymaps({{
+   mode        = 'n',
+   keys        = '<leader>cls',
+   command     = '<cmd>SymbolsOutline<CR>',
+   description = 'Show symbols',
+}})
