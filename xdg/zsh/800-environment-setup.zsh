e0e6a93ec929bd53d0c981bd08d5ab0450b63a1c
scheatkode <scheatkode@gmail.com>
2021-11-06 18:00:07 +0100

    :bug: fix(zsh): Make neovim the default editor

--- xdg/zsh/800-environment-setup.zsh
+++ xdg/zsh/800-environment-setup.zsh
@@ -4,6 +4,14 @@
 #                    ░▀▀▀░▀░▀░░▀░░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░░                      #
 # -------------------------------------------------------------------------- #
 
+# EDITOR
+
+if command -v nvim > /dev/null 2>&1 ; then
+   export EDITOR='nvim'
+fi
+
+# Dotnet
+
 export DOTNET_CLI_TELEMETRY_OPTOUT=yes
 export              DOTNET_NOLOGO=yes
 
