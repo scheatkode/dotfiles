13cd56f3c2c652e7fb03b34f62ed4e58a0184258
scheatkode <scheatkode@gmail.com>
2021-11-10 11:34:50 +0100

    :bug: fix(p/symbols): Update symbols outline keymaps

--- xdg/neovim/lua/plugins/symbols/init.lua
+++ xdg/neovim/lua/plugins/symbols/init.lua
@@ -1,8 +1,10 @@
 return {'simrat39/symbols-outline.nvim',
    cmd      = { 'SymbolsOutline'        },
-   keys     = { '<leader>cls'           },
+   keys     = { '<leader>co'            },
+
    wants    = { 'nvim-lspconfig'        },
    requires = { 'neovim/nvim-lspconfig' },
+
    config   = function ()
       require('plugins.symbols.config')
       require('plugins.symbols.keys')
--- xdg/neovim/lua/plugins/symbols/keys.lua
+++ xdg/neovim/lua/plugins/symbols/keys.lua
@@ -1,6 +1,6 @@
 return require('util').register_keymaps({{
    mode        = 'n',
-   keys        = '<leader>cls',
+   keys        = '<leader>co',
    command     = '<cmd>SymbolsOutline<CR>',
-   description = 'Show symbols',
+   description = 'Code outline',
 }})
