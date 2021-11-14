e3ce76580fdc61611e323e8ec4c1187816dd4b7b
scheatkode <scheatkode@gmail.com>
2021-11-14 15:51:15 +0100

    :bug: fix(p/pairs): Remove deprecated setup configuration

--- xdg/neovim/lua/plugins/pairs/config.lua
+++ xdg/neovim/lua/plugins/pairs/config.lua
@@ -1,20 +1,11 @@
 local log = require('log')
 
 local has_pairs,      pairs  = pcall(require, 'nvim-autopairs')
-local has_engine,     engine = pcall(require, 'nvim-autopairs.completion.cmp')
 local has_treesitter, _      = pcall(require, 'nvim-treesitter.configs')
 
 if not has_pairs then
    log.error('Tried loading plugin ... unsuccessfully ‼', 'nvim-autopairs')
    return has_pairs
-end
-
-if has_engine then
-   engine.setup {
-      map_cr       = true,
-      map_complete = true,
-      auto_select  = true,
-   }
 end
 
 if has_treesitter then
