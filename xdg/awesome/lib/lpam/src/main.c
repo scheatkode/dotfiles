ec22d69757b2028f703cc1fd8f6bc7795e4fc753
scheatkode <scheatkode@gmail.com>
2021-12-19 14:38:14 +0100

    :recycle: refactor(awesome): Refactor PAM module
    pam_authenticate() used to cause a segmentation fault when the number of
    allowed failures is  exceeded, which is a critical  security issue since
    AwesomeWM crashes  and we're  back to  the tty on  which startx  was run
    without the need to provide a correct password

--- xdg/awesome/lib/lpam/src/main.c
+++ xdg/awesome/lib/lpam/src/main.c
@@ -1,14 +1,17 @@
-#include <pwd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <errno.h>
+#include <pwd.h>               //! `getpwuid()`
+#include <stdlib.h>            //! `malloc()`
+#include <string.h>            //! `strlen()` and `memcpy()`
+#include <unistd.h>            //! `getuid()`
 
-#include <security/pam_appl.h>
+#include <security/pam_appl.h> //! `pam_*()` and `PAM_*`
 
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+#include "lauxlib.h"           //! `lua_*` and `luaL_*`
 
+/*!
+ * Different  Lua  versions  have  different  exporting
+ * mechanisms.
+ */
 #if LUA_VERSION_NUM == 501
 
 #  define export_module(L, l) luaL_register(L, "lpam", l)
@@ -31,44 +34,64 @@
 
 #endif
 
-static struct pam_response * reply;
-
-static char * strdup(const char * source)
-{
-   size_t len = strlen(source) + 1;
-   char * new = (char *) malloc(len);
-
-   if (new == NULL) {
-      return new;
-   }
-
-   memcpy(new, source, len);
-   return new;
-}
-
+/*!
+ * Helper function  to verify whether the  given string
+ * `s` is null  or empty. Empty meaning  it contains no
+ * characters.
+ */
 inline static int is_null_or_empty(const char * s) {
    return s == NULL || s[0] == '\0';
 }
 
+/*!
+ * Dummy conversation function which  only sets the pam
+ * response struct with the stored password.
+ */
 static int converse_for_authentication(
    int                         message_number,
    const struct pam_message ** messages,
    struct pam_response **      response,
    void *                      application_data
 ) {
-   (void) message_number;
    (void) messages;
-   (void) application_data;
 
+   const char * password = (const char *) application_data;
+
+   if (
+         message_number != 1
+      || is_null_or_empty(password)
+   ) {
+      return PAM_CONV_ERR;
+   }
+
+   struct pam_response * reply;
+
+   if (NULL == (reply = malloc(sizeof(struct pam_response)))) {
+      return PAM_BUF_ERR;
+   }
+
+   const size_t size = strlen(password) + 1;
+
+   if (NULL == (reply->resp = malloc(size))) {
+      free(reply);
+      return PAM_BUF_ERR;
+   }
+
+   strncpy(reply->resp, password, size);
+
+   reply->resp_retcode = 0;
    *response = reply;
 
    return PAM_SUCCESS;
 }
 
+/*!
+ * This function  is exported  to the Lua  runtime and,
+ * given  a password,  authenticates  the current  user
+ * using the PAM builtin "login" service.
+ */
 static int authenticate_current_user(lua_State * L)
 {
-   int return_value;
-
    const char * password = luaL_checkstring(L, -1);
 
    if (is_null_or_empty(password)) {
@@ -76,42 +99,48 @@ static int authenticate_current_user(lua_State * L)
       return 1;
    }
 
-   struct passwd * passwd     = getpwuid(getuid());
-   const char    * username   = passwd->pw_name;
-   pam_handle_t  * pam_handle = NULL;
-
    struct pam_conv conversation = {
-      converse_for_authentication, NULL
+      .appdata_ptr = (void *) password,
+      .conv        = converse_for_authentication,
    };
 
-   if (NULL == (reply = calloc(1, sizeof(struct pam_response)))) {
-      lua_pushboolean(L, 0);
-      return 1;
-   }
+   struct passwd * passwd     = getpwuid(getuid());
+   const  char   * username   = passwd->pw_name;
+   pam_handle_t  * pam_handle = NULL;
 
    if (PAM_SUCCESS != pam_start("login", username, &conversation, &pam_handle)) {
       lua_pushboolean(L, 0);
       return 1;
    }
 
-   reply->resp = strdup(password);
-   reply->resp_retcode = 0;
+   int return_value = 1;
 
-   if (PAM_SUCCESS != (return_value = pam_authenticate(pam_handle, 0))) {
-      lua_pushboolean(L, 0);
-      return 1;
+   if (
+         PAM_SUCCESS != pam_authenticate(pam_handle, PAM_DISALLOW_NULL_AUTHTOK)
+      || PAM_SUCCESS !=    pam_acct_mgmt(pam_handle, PAM_DISALLOW_NULL_AUTHTOK)
+   ) {
+      return_value = 0;
    }
 
-   lua_pushboolean(L, 1);
-   pam_end(pam_handle, return_value);
+   pam_end(pam_handle, PAM_SUCCESS);
+   lua_pushboolean(L, return_value);
 
    return 1;
 }
 
+/*!
+ * This object  stores the  functions available  to Lua
+ * when exporting the module.
+ */
 static const struct luaL_Reg lpam[] = {
-   {"authenticate_current_user", authenticate_current_user}, {NULL, NULL}
+   { "authenticate_current_user", authenticate_current_user },
+   { NULL,                        NULL                      },
 };
 
+/*!
+ * This function  exports the  object defined  above to
+ * the Lua runtime.
+ */
 int luaopen_liblpam(lua_State * L)
 {
    export_module(L, lpam);
