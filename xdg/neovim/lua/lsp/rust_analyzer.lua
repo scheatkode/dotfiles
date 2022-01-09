91b0b80f99dbab8b57e1c1b21b15b45b1dbf2856
scheatkode <scheatkode@gmail.com>
2022-01-09 23:42:18 +0100

    :wrench: fix(lsp/rust_analyzer): Update LSP configuration

--- xdg/neovim/lua/lsp/rust_analyzer.lua
+++ xdg/neovim/lua/lsp/rust_analyzer.lua
@@ -1,5 +1,12 @@
+local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
+
+if not has_lspconfig then
+   print('‼ Tried loading lspconfig for rust_analyzer ... unsuccessfully.')
+   return has_lspconfig
+end
+
 return {
-   cmd = { 'rust-analyzer' },
+   cmd = { vim.fn.stdpath('data') .. '/lsp_servers/rust/rust-analyzer' },
    filetypes = { 'rust' },
    settings = {
       ['rust-analyzer'] = {},
