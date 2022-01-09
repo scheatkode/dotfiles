f7b4deb67666ce55a52f28bd7dbfb38f5a973d36
scheatkode <scheatkode@gmail.com>
2022-01-09 23:42:06 +0100

    :wrench: fix(lsp/omnisharp): Update LSP configuration

--- xdg/neovim/lua/lsp/omnisharp.lua
+++ xdg/neovim/lua/lsp/omnisharp.lua
@@ -1,9 +1,88 @@
-local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
+local f = require('f')
+
+local has_lspconfig, _ = pcall(require, 'lspconfig')
 
 if not has_lspconfig then
    print('‼ Tried loading lspconfig for omnisharp ... unsuccessfully.')
    return has_lspconfig
 end
+
+local function omnisharp_options_to_env(t)
+   local env = {}
+
+   f.iterate(t):foreach(function (namespace, values)
+      f.iterate(values):foreach(function (key, value)
+         env[string.format('OMNISHARP_%s:%s', namespace, key)] = value
+      end)
+   end)
+
+   return env
+end
+
+local omnisharp_env = {
+   formattingOptions = {
+      enableEditorConfigSupport = true,
+      indentBlock = true,
+      indentBraces = false,
+      indentSwitchCaseSection = true,
+      indentSwitchCaseSectionWhenBlock = true,
+      indentSwitchSection = true,
+      indentationSize = 3,
+      labelPositioning = 'oneLess',
+      newLine = '\n',
+      newLineForCatch = true,
+      newLineForClausesInQuery = true,
+      newLineForElse = false,
+      newLineForFinally = true,
+      newLineForMembersInAnonymousTypes = true,
+      newLineForMembersInObjectInit = true,
+      newLinesForBracesInAccessors = true,
+      newLinesForBracesInAnonymousMethods = false,
+      newLinesForBracesInAnonymousTypes = false,
+      newLinesForBracesInControlBlocks = false,
+      newLinesForBracesInLambdaExpressionBody = false,
+      newLinesForBracesInMethods = true,
+      newLinesForBracesInObjectCollectionArrayInitializers = true,
+      newLinesForBracesInProperties = true,
+      newLinesForBracesInTypes = true,
+      organizeImports = true,
+      spaceAfterCast = true,
+      spaceAfterColonInBaseTypeDeclaration = true,
+      spaceAfterComma = true,
+      spaceAfterControlFlowStatementKeyword = true,
+      spaceAfterDot = false,
+      spaceAfterMethodCallName = false,
+      spaceAfterSemicolonsInForStatement = true,
+      spaceBeforeColonInBaseTypeDeclaration = true,
+      spaceBeforeComma = false,
+      spaceBeforeDot = false,
+      spaceBeforeOpenSquareBracket = false,
+      spaceBeforeSemicolonsInForStatement = false,
+      spaceBetweenEmptyMethodCallParentheses = false,
+      spaceBetweenEmptyMethodDeclarationParentheses = false,
+      spaceBetweenEmptySquareBrackets = false,
+      spaceWithinCastParentheses = false,
+      spaceWithinExpressionParentheses = false,
+      spaceWithinMethodCallParentheses = false,
+      spaceWithinMethodDeclarationParenthesis = false,
+      spaceWithinOtherParentheses = false,
+      spaceWithinSquareBrackets = true,
+      spacesIgnoreAroundVariableDeclaration = false,
+      spacingAfterMethodDeclarationName = true,
+      spacingAroundBinaryOperator = 'single',
+      tabSize = 3,
+      useTabs = false,
+      wrappingKeepStatementsOnSingleLine = true,
+      wrappingPreserveSingleLine = true,
+   },
+
+   roslynExtensionsOptions = {
+      documentAnalysisTimeoutMs  = 10000,
+      enableAnalyzersSupport     = true,
+      enableDecompilationSupport = true,
+      enableImportCompletion     = true,
+   },
+}
 
 return {
    cmd = {
@@ -17,65 +96,9 @@ return {
       'vb'
    },
 
-   init_options = {
-      FormattingOptions = {
-         EnableEditorConfigSupport = true,
-         OrganizeImports = true,
-         NewLine = '\n',
-         UseTabs = false,
-         TabSize = 3,
-         IndentationSize = 3,
-         SpacingAfterMethodDeclarationName = true,
-         SpaceWithinMethodDeclarationParenthesis = false,
-         SpaceBetweenEmptyMethodDeclarationParentheses = false,
-         SpaceAfterMethodCallName = false,
-         SpaceWithinMethodCallParentheses = false,
-         SpaceBetweenEmptyMethodCallParentheses = false,
-         SpaceAfterControlFlowStatementKeyword = true,
-         SpaceWithinExpressionParentheses = false,
-         SpaceWithinCastParentheses = false,
-         SpaceWithinOtherParentheses = false,
-         SpaceAfterCast = true,
-         SpacesIgnoreAroundVariableDeclaration = false,
-         SpaceBeforeOpenSquareBracket = false,
-         SpaceBetweenEmptySquareBrackets = false,
-         SpaceWithinSquareBrackets = true,
-         SpaceAfterColonInBaseTypeDeclaration = true,
-         SpaceAfterComma = true,
-         SpaceAfterDot = false,
-         SpaceAfterSemicolonsInForStatement = true,
-         SpaceBeforeColonInBaseTypeDeclaration = true,
-         SpaceBeforeComma = false,
-         SpaceBeforeDot = false,
-         SpaceBeforeSemicolonsInForStatement = false,
-         SpacingAroundBinaryOperator = 'single',
-         IndentBraces = false,
-         IndentBlock = true,
-         IndentSwitchSection = true,
-         IndentSwitchCaseSection = true,
-         IndentSwitchCaseSectionWhenBlock = true,
-         LabelPositioning = 'oneLess',
-         WrappingPreserveSingleLine = true,
-         WrappingKeepStatementsOnSingleLine = true,
-         NewLinesForBracesInTypes = true,
-         NewLinesForBracesInMethods = true,
-         NewLinesForBracesInProperties = true,
-         NewLinesForBracesInAccessors = true,
-         NewLinesForBracesInAnonymousMethods = true,
-         NewLinesForBracesInControlBlocks = true,
-         NewLinesForBracesInAnonymousTypes = true,
-         NewLinesForBracesInObjectCollectionArrayInitializers = true,
-         NewLinesForBracesInLambdaExpressionBody = true,
-         NewLineForElse = true,
-         NewLineForCatch = true,
-         NewLineForFinally = true,
-         NewLineForMembersInObjectInit = true,
-         NewLineForMembersInAnonymousTypes = true,
-         NewLineForClausesInQuery = true,
-      }
-   },
+   cmd_env = omnisharp_options_to_env(omnisharp_env),
 
-   root_dir = lspconfig.util.root_pattern('**/*.sln')
-           or lspconfig.util.root_pattern('**/*.csproj')
-           or lspconfig.util.root_pattern('.git'),
+   -- root_dir = lspconfig.util.root_pattern('**/*.sln')
+   --         or lspconfig.util.root_pattern('**/*.csproj')
+   --         or lspconfig.util.root_pattern('.git'),
 }
