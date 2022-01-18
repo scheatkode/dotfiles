b882035c6ca6a739122e8177a0fb84fb4d9c5bbb
scheatkode <scheatkode@gmail.com>
2022-01-18 22:52:55 +0100

    :broom: feat(zsh): Move `less` files

---
+++ xdg/zsh/009-less-setup.zsh
@@ -0,0 +1,6 @@
+#       ░█░░░█▀▀░█▀▀░█▀▀░░░█▀▀░█▀▀░▀█▀░█░█░█▀█
+#       ░█░░░█▀▀░▀▀█░▀▀█░░░▀▀█░█▀▀░░█░░█░█░█▀▀
+#       ░▀▀▀░▀▀▀░▀▀▀░▀▀▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░░
+
+export LESSHISTFILE="${XDG_DATA_HOME}/less/history"
+export LESSKEY="${XDG_DATA_HOME}/less/keys"
