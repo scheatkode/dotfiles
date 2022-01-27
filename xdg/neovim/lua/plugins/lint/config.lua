17e12422725f7f572a492de5104a150349a6dc73
scheatkode <scheatkode@gmail.com>
2022-01-27 21:06:11 +0100

    :poop: chore(neovim/lint): Update linter configurations

--- xdg/neovim/lua/plugins/lint/config.lua
+++ xdg/neovim/lua/plugins/lint/config.lua
@@ -1,50 +1,125 @@
 local has_ls, ls = pcall(require, 'null-ls')
+local log = require('log')
 
 if not has_ls then
-   print('something') -- TODO
+   log.error('Tried loading plugin ... unsuccessfully ‼', 'null-ls')
    return has_ls
 end
 
 local prettier = ls.builtins.formatting.prettier.with({
-   'html',
-   'css',
-   'scss',
+   command = vim.fn.expand('./node_modules/.bin/prettier'),
 
-   'markdown',
+   args = {
+      '--stdin-filepath', '$FILENAME',
+      '--tab-width', '3',
+      '--no-semi',
+      '--single-quote',
+      '--jsx-single-quote',
+   },
 
-   'javascript',
-   'javascript.jsx',
-   'javascriptreact',
+   filetypes = {
+      'html',
+      'css',
+      'scss',
 
-   'json',
+      'markdown',
 
-   'typescript',
-   'typescript.tsx',
-   'typescriptreact',
+      'javascript',
+      'javascript.jsx',
+      'javascriptreact',
 
-   'vue',
-   'svelte',
+      'json',
 
-   'yaml',
+      'typescript',
+      'typescript.tsx',
+      'typescriptreact',
+
+      'vue',
+      'svelte',
+
+      'yaml',
+}})
+
+local eslint_diagnostics = ls.builtins.diagnostics.eslint.with({
+   command = vim.fn.expand('./node_modules/.bin/eslint_d'),
+
+   args = {
+      '-f',
+      'json',
+      '--config', './eslint.config.js',
+      '--stdin',
+      '--stdin-filename', '$FILENAME',
+   },
+
+   filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'vue',
+   },
 })
 
--- local stylua = ls.builtins.formatting.stylua.with({
+local eslint_codeactions = ls.builtins.code_actions.eslint.with({
+   command = vim.fn.expand('./node_modules/.bin/eslint_d'),
 
--- })
+   args = {
+      '-f',
+      'json',
+      '--config', './eslint.config.js',
+      '--stdin',
+      '--stdin-filename', '$FILENAME',
+   },
 
--- local vale = ls.builtins.diagnostics.vale.with({
---    'markdown',
---    'tex',
--- })
+   filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'vue',
+   },
+})
 
-ls.config {
+
+local stylua = ls.builtins.formatting.stylua.with({
+
+})
+
+local vale = ls.builtins.diagnostics.vale.with({
+   'markdown',
+   'tex',
+})
+
+local hadolint = ls.builtins.diagnostics.hadolint.with({
+   command = 'podman',
+   args = {
+      'run',
+      '--rm',
+      '-iv',
+      vim.loop.cwd() .. ':' .. vim.loop.cwd(),
+      'docker.io/hadolint/hadolint',
+      'hadolint',
+      '--no-fail',
+      '--format=json',
+      '$FILENAME',
+   },
+})
+
+ls.setup({
              debounce = 250,
       default_timeout = 5000,
    diagnostics_format = '#{m}',
 
+   on_attach = require('plugins.lspconfig.on_attach'),
+
+   root_dir = vim.loop.cwd,
+
    sources = {
+      eslint_codeactions,
+      eslint_diagnostics,
+      hadolint,
       prettier,
       -- stylua,
       -- vale,
    }
-}
+})
