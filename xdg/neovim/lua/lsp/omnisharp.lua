8e916273340566c84ae9e3409efa66fad480bbe3
scheatkode <scheatkode@gmail.com>
2022-02-10 20:19:33 +0100

    :recycle: refactor(neovim): Refactor OmniSharp configuration
    Refactor OmniSharp's startup configuration :
       - Use a generalized table flattening function
       - Add useModernNet setting for .NET SDK development
       - Restore custom root directory detection

--- xdg/neovim/lua/lsp/omnisharp.lua
+++ xdg/neovim/lua/lsp/omnisharp.lua
@@ -1,92 +1,137 @@
-local f = require('f')
+local tablex = require('tablex')
 
-local has_lspconfig, _ = pcall(require, 'lspconfig')
+local sf   = string.format
+local til  = tablex.is_list
+local type = type
+
+local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
 
 if not has_lspconfig then
    print('‼ Tried loading lspconfig for omnisharp ... unsuccessfully.')
    return has_lspconfig
 end
 
-local function omnisharp_options_to_env(t)
-   local env = {}
+--- Recursively flatten a table.
+---
+--- @param acc table Accumulator of flattened keys
+--- @param sep string Separator to use for keys
+--- @param key string Current key
+--- @param value any Current value
+local function flatten (acc, sep, key, value)
+   if type(value) ~= 'table' then
+      acc[key] = value
+      return acc
+   end
 
-   f.iterate(t):foreach(function (namespace, values)
-      f.iterate(values):foreach(function (key, value)
-         env[string.format('OMNISHARP_%s:%s', namespace, key)] = value
-      end)
-   end)
+   if til(value) then
+      for i = 1, #value do
+         flatten(
+            acc,
+            ':',
+            sf('%s%s%s', key, sep, i - 1),
+            value[i]
+         )
+      end
 
-   return env
+      return acc
+   end
+
+   for k, v in pairs(value) do
+      flatten(
+         acc,
+         ':',
+         sf('%s%s%s', key, sep, k),
+         v
+      )
+   end
+
+   return acc
 end
 
-local omnisharp_env = {
-   formattingOptions = {
-      enableEditorConfigSupport = true,
-      indentBlock = true,
-      indentBraces = false,
-      indentSwitchCaseSection = true,
-      indentSwitchCaseSectionWhenBlock = true,
-      indentSwitchSection = true,
-      indentationSize = 3,
-      labelPositioning = 'oneLess',
-      newLine = '\n',
-      newLineForCatch = true,
-      newLineForClausesInQuery = true,
-      newLineForElse = false,
-      newLineForFinally = true,
-      newLineForMembersInAnonymousTypes = true,
-      newLineForMembersInObjectInit = true,
-      newLinesForBracesInAccessors = true,
-      newLinesForBracesInAnonymousMethods = false,
-      newLinesForBracesInAnonymousTypes = false,
-      newLinesForBracesInControlBlocks = false,
-      newLinesForBracesInLambdaExpressionBody = false,
-      newLinesForBracesInMethods = true,
-      newLinesForBracesInObjectCollectionArrayInitializers = true,
-      newLinesForBracesInProperties = true,
-      newLinesForBracesInTypes = true,
-      organizeImports = true,
-      spaceAfterCast = true,
-      spaceAfterColonInBaseTypeDeclaration = true,
-      spaceAfterComma = true,
-      spaceAfterControlFlowStatementKeyword = true,
-      spaceAfterDot = false,
-      spaceAfterMethodCallName = false,
-      spaceAfterSemicolonsInForStatement = true,
-      spaceBeforeColonInBaseTypeDeclaration = true,
-      spaceBeforeComma = false,
-      spaceBeforeDot = false,
-      spaceBeforeOpenSquareBracket = false,
-      spaceBeforeSemicolonsInForStatement = false,
-      spaceBetweenEmptyMethodCallParentheses = false,
-      spaceBetweenEmptyMethodDeclarationParentheses = false,
-      spaceBetweenEmptySquareBrackets = false,
-      spaceWithinCastParentheses = false,
-      spaceWithinExpressionParentheses = false,
-      spaceWithinMethodCallParentheses = false,
-      spaceWithinMethodDeclarationParenthesis = false,
-      spaceWithinOtherParentheses = false,
-      spaceWithinSquareBrackets = true,
-      spacesIgnoreAroundVariableDeclaration = false,
-      spacingAfterMethodDeclarationName = true,
-      spacingAroundBinaryOperator = 'single',
-      tabSize = 3,
-      useTabs = false,
-      wrappingKeepStatementsOnSingleLine = true,
-      wrappingPreserveSingleLine = true,
-   },
+--- OmniSharp's settings as a table
+---
+--- Using  a function's  return  value  as a  temporary
+--- variable to  avoid the  table lingering  around the
+--- memory after  its use,  when the  garbage collector
+--- sweeps for unreferenced objects.
+---
+--- @return table OmniSharp settings table
+local function omnisharp_settings ()
+   return {
+      useModernNet = true,
 
-   roslynExtensionsOptions = {
-      documentAnalysisTimeoutMs  = 10000,
-      enableAnalyzersSupport     = true,
-      enableDecompilationSupport = true,
-      enableImportCompletion     = true,
-   },
-}
+      formattingOptions = {
+         enableEditorConfigSupport = true,
+         indentBlock = true,
+         indentBraces = false,
+         indentSwitchCaseSection = true,
+         indentSwitchCaseSectionWhenBlock = true,
+         indentSwitchSection = true,
+         indentationSize = 3,
+         labelPositioning = 'oneLess',
+         newLine = '\n',
+         newLineForCatch = true,
+         newLineForClausesInQuery = true,
+         newLineForElse = false,
+         newLineForFinally = true,
+         newLineForMembersInAnonymousTypes = true,
+         newLineForMembersInObjectInit = true,
+         newLinesForBracesInAccessors = true,
+         newLinesForBracesInAnonymousMethods = false,
+         newLinesForBracesInAnonymousTypes = false,
+         newLinesForBracesInControlBlocks = false,
+         newLinesForBracesInLambdaExpressionBody = false,
+         newLinesForBracesInMethods = true,
+         newLinesForBracesInObjectCollectionArrayInitializers = true,
+         newLinesForBracesInProperties = true,
+         newLinesForBracesInTypes = true,
+         organizeImports = true,
+         spaceAfterCast = true,
+         spaceAfterColonInBaseTypeDeclaration = true,
+         spaceAfterComma = true,
+         spaceAfterControlFlowStatementKeyword = true,
+         spaceAfterDot = false,
+         spaceAfterMethodCallName = false,
+         spaceAfterSemicolonsInForStatement = true,
+         spaceBeforeColonInBaseTypeDeclaration = true,
+         spaceBeforeComma = false,
+         spaceBeforeDot = false,
+         spaceBeforeOpenSquareBracket = false,
+         spaceBeforeSemicolonsInForStatement = false,
+         spaceBetweenEmptyMethodCallParentheses = false,
+         spaceBetweenEmptyMethodDeclarationParentheses = false,
+         spaceBetweenEmptySquareBrackets = false,
+         spaceWithinCastParentheses = false,
+         spaceWithinExpressionParentheses = false,
+         spaceWithinMethodCallParentheses = false,
+         spaceWithinMethodDeclarationParenthesis = false,
+         spaceWithinOtherParentheses = false,
+         spaceWithinSquareBrackets = true,
+         spacesIgnoreAroundVariableDeclaration = false,
+         spacingAfterMethodDeclarationName = true,
+         spacingAroundBinaryOperator = 'single',
+         tabSize = 3,
+         useTabs = false,
+         wrappingKeepStatementsOnSingleLine = true,
+         wrappingPreserveSingleLine = true,
+      },
+
+      roslynExtensionsOptions = {
+         documentAnalysisTimeoutMs  = 10000,
+         enableAnalyzersSupport     = true,
+         enableDecompilationSupport = true,
+         enableImportCompletion     = true,
+      },
+   }
+end
 
 return {
    cmd = {
-      vim.fn.stdpath('data') .. '/lsp_servers/omnisharp/omnisharp/run',
+      sf(
+         '%s/lsp_servers/omnisharp/OmniSharp',
+         vim.fn.stdpath('data')
+      ),
+
       '--languageserver',
       '--hostPID', tostring(vim.fn.getpid())
    },
@@ -96,9 +141,8 @@ return {
       'vb'
    },
 
-   cmd_env = omnisharp_options_to_env(omnisharp_env),
-
-   -- root_dir = lspconfig.util.root_pattern('**/*.sln')
-   --         or lspconfig.util.root_pattern('**/*.csproj')
-   --         or lspconfig.util.root_pattern('.git'),
+   cmd_env  = flatten({}, 'OMNISHARP', '_', omnisharp_settings()),
+   root_dir = lspconfig.util.root_pattern('**/*.sln')
+           or lspconfig.util.root_pattern('**/*.csproj')
+           or lspconfig.util.root_pattern('.git'),
 }
