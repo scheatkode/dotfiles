2e97cd5c7c80e565ef1246530a31092205e373a8
scheatkode <scheatkode@gmail.com>
2022-02-11 16:59:16 +0100

    :recycle: refactor(neovim): Refactor `lspconfig` configuration

--- xdg/neovim/lua/plugins/lspconfig/config.lua
+++ xdg/neovim/lua/plugins/lspconfig/config.lua
@@ -1,418 +1,45 @@
---- localise vim globals
-
-local api = vim.api
-local lsp = vim.lsp
-
 --- required and optional dependencies
+
+local f     = require('f')
+local typex = require('typex')()
+
+local on_attach = require('plugins.lspconfig.on_attach')
 
 local has_lspinstall, lspinstall = pcall(require, 'nvim-lsp-installer')
 local has_lspconfig,  lspconfig  = pcall(require, 'lspconfig')
-local has_whichkey,   whichkey   = pcall(require, 'which-key')
 
-local register_keymap = require('util').register_single_keymap
-local log             = require('log')
+local log = require('log')
 
 --- globals
 
 local text_change_default_debounce = 150
 local languages = {
+   powershell = 'powershell_es',
    python = 'pyright',
    -- 'gopls',
    rust =  'rust_analyzer',
-   -- javascript = 'tsserver',
+   javascript = 'tsserver',
    -- 'vimls',
    bash   = 'bashls',
    cpp    = 'ccls',
-   deno   = 'denols',
+   -- deno   = 'denols',
    php    = 'intelephense',
    java   = 'jdtls',
    dart = 'dartls',
    json   = 'jsonls',
+   -- csharp = 'csharp_ls',
    csharp = 'omnisharp',
    -- 'perlls',
    -- 'perlpls',
    lua    = 'sumneko_lua',
    -- 'phpactor',
    yaml   = 'yamlls',
+   cmake = 'cmake',
 
    tailwindcss = 'tailwindcss',
 
-   generic = 'null-ls',
-
+   salt_ls = 'salt_ls'
 }
-
-local border = {
-
-   --- rounded border
-   {'╭', 'FloatBorder'},
-   {'─', 'FloatBorder'},
-   {'╮', 'FloatBorder'},
-   {'│', 'FloatBorder'},
-   {'╯', 'FloatBorder'},
-   {'─', 'FloatBorder'},
-   {'╰', 'FloatBorder'},
-   {'│', 'FloatBorder'},
-
-   --- square border
-   -- {'┌', 'FloatBorder'},
-   -- {'─', 'FloatBorder'},
-   -- {'┐', 'FloatBorder'},
-   -- {'│', 'FloatBorder'},
-   -- {'┘', 'FloatBorder'},
-   -- {'─', 'FloatBorder'},
-   -- {'└', 'FloatBorder'},
-   -- {'│', 'FloatBorder'},
-
-}
-
--- mappings registration shorthand {{{1
-
-local normalize_keymaps = function (mappings)
-   for _, mapping in ipairs(mappings) do
-      local mode         = mapping.mode
-      local keys         = mapping.keys
-      local description  = mapping.description
-      local command      = mapping.command
-      local condition    = mapping.condition
-      local options      = mapping.options
-
-      local is_command_valid =
-             command ~= nil
-         and command ~= false
-         and command ~= ''
-
-      local is_condition_valid = condition ~= nil and condition ~= false
-
-      if is_command_valid and is_condition_valid then
-         register_keymap {
-            mode    = mode,
-            keys    = keys,
-            command = command,
-            options = options,
-         }
-
-         if has_whichkey then
-            whichkey.register({
-               [keys] = description
-            }, {
-               mode   = mode,
-               buffer = options.buffer
-            })
-         end
-      end
-   end
-end
-
--- on attach {{{1
-
-local on_attach = function (client, bufnr)
-
-   -- omnifunc setup {{{2
-
-   vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-   -- border customization {{{2
-
-   lsp.handlers['textDocument/hover']         = lsp.with(lsp.handlers.hover, {border = border})
-   lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.hover, {border = border})
-
-   -- mappings {{{2
-
-   normalize_keymaps {
-
-      -- declaration {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clD',
-         description  = 'Declaration',
-         command      = '<cmd>lua vim.lsp.buf.declaration()<CR>',
-         condition    = client.resolved_capabilities.declaration,
-         options      = { buffer = bufnr },
-      },
-
-      -- definition {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>cld',
-         description  = 'Definition',
-         command      = '<cmd>lua vim.lsp.buf.definition()<CR>',
-         condition    = client.resolved_capabilities.goto_definition,
-         options      = { buffer = bufnr },
-      },
-
-      -- type definition {{{3
-
-      {
-         mode         = 'n',
-         keys         =  '<leader>clT',
-         description  = 'Type definition',
-         command      = '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-         condition    = client.resolved_capabilities.type_definition,
-         options      = { buffer = bufnr },
-      },
-
-      -- implementation {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>cli',
-         description  = 'Implementation',
-         command      = '<cmd>lua vim.lsp.buf.implementation()<CR>',
-         condition    = client.resolved_capabilities.implementation,
-         options      = { buffer = bufnr },
-      },
-
-      -- rename symbol {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clR',
-         description  = 'Rename symbol',
-         command      = '<cmd>lua vim.lsp.buf.rename()<CR>',
-         condition    = client.resolved_capabilities.rename,
-         options      = { buffer = bufnr },
-      },
-
-      -- references {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clr',
-         description  = 'References',
-         command      = '<cmd>lua vim.lsp.buf.references()<CR>',
-         condition    = client.resolved_capabilities.references,
-         options      = { buffer = bufnr },
-      },
-
-      -- code action {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>cla',
-         description  = 'Code action',
-         command      = '<cmd>lua vim.lsp.buf.code_action()<CR>',
-         condition    = client.resolved_capabilities.code_action,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'x',
-         keys         = '<leader>cla',
-         description  = 'Code action',
-         command      = '<cmd>lua vim.lsp.buf.code_action()<CR>',
-         condition    = client.resolved_capabilities.code_action,
-         options      = { buffer = bufnr },
-      },
-
-      -- hover documentation {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clh',
-         description  = 'Hover documentation',
-         command      = '<cmd>lua vim.lsp.buf.hover()<CR>',
-         condition    = client.resolved_capabilities.hover,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = 'K',
-         description  = 'Hover documentation',
-         command      = '<cmd>lua vim.lsp.buf.hover()<CR>',
-         condition    = client.resolved_capabilities.hover,
-         options      = { buffer = bufnr },
-      },
-
-      -- call hierarchy {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clI',
-         description  = 'Incoming calls',
-         command      = '<cmd>lua vim.lsp.buf.incoming_calls()<CR>',
-         condition    = client.resolved_capabilities.call_hierarchy,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clO',
-         description  = 'Outgoing calls',
-         command      = '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>',
-         condition    = client.resolved_capabilities.call_hierarchy,
-         options      = { buffer = bufnr },
-      },
-
-      -- diagnostics {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>cll',
-         description  = 'Line diagnostics',
-         command      = '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border = "rounded"})<CR>',
-         condition    = true,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clL',
-         description  = 'Send diagnostics to loclist',
-         command      = '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
-         condition    = true,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '[d',
-         description  = 'Go to previous diagnostic',
-         command      = '<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts = {border = "rounded"}})<CR>',
-         condition    = true,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = ']d',
-         description  = 'Go to next diagnostic',
-         command      = '<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts = {border = "rounded"}})<CR>',
-         condition    = true,
-         options      = { buffer = bufnr },
-      },
-
-      -- signature help {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clS',
-         description  = 'Signature help',
-         command      = '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-         condition    = client.resolved_capabilities.signature_help,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'i',
-         keys         = '<M-s>',
-         description  = 'Signature help',
-         command      = '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-         condition    = client.resolved_capabilities.signature_help,
-         options      = { buffer = bufnr },
-      },
-
-      -- workspace management {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clwa',
-         description  = 'Add folder to workspace',
-         command      = '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
-         condition    = client.resolved_capabilities.workspace_folder_properties,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clwd',
-         description  = 'Remove folder from workspace',
-         command      = '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
-         condition    = client.resolved_capabilities.workspace_folder_properties,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '<leader>clwl',
-         description  = 'List folders in workspace',
-         command      = '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-         condition    = client.resolved_capabilities.workspace_folder_properties,
-         options      = { buffer = bufnr },
-      },
-
-      -- code formatting {{{3
-
-      {
-         mode         = 'n',
-         keys         = '<leader>cf',
-         description  = 'Format code',
-         command      = '<cmd>lua vim.lsp.buf.formatting()<CR>',
-         condition    = client.resolved_capabilities.document_formatting
-            or client.resolved_capabilities.document_range_formatting,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'n',
-         keys         = '<leader>=',
-         description  = 'Format code',
-         command      = '<cmd>lua vim.lsp.buf.formatting()<CR>',
-         condition    = client.resolved_capabilities.document_formatting
-            or client.resolved_capabilities.document_range_formatting,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'x',
-         keys         = '<leader>cf',
-         description  = 'Format code',
-         command      = '<cmd>lua vim.lsp.buf.formatting()<CR>',
-         condition    = client.resolved_capabilities.document_formatting
-            or client.resolved_capabilities.document_range_formatting,
-         options      = { buffer = bufnr },
-      },
-
-      {
-         mode         = 'x',
-         keys         = '<leader>=',
-         description  = 'Format code',
-         command      = '<cmd>lua vim.lsp.buf.formatting()<CR>',
-         condition    = client.resolved_capabilities.document_formatting
-            or client.resolved_capabilities.document_range_formatting,
-         options      = { buffer = bufnr },
-      },
-
-      -- }}}
-
-   }
-
-   -- mapping category names
-
-   if has_whichkey then
-      whichkey.register({
-         ['<leader>cl'] = {
-            name = '+lsp',
-            w = {
-               name = '+workspace',
-            },
-         },
-      }, {
-         buffer = bufnr
-      })
-   end
-
-   -- autocommands {{{2
-
-   if client.resolved_capabilities.document_highlight then
-      api.nvim_exec([[
-         :hi LspReferenceRead  cterm=bold ctermbg=red guibg=LightYellow
-         :hi LspReferenceText  cterm=bold ctermbg=red guibg=LightYellow
-         :hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-
-         augroup lsp_document_highlight
-         autocmd! *           <buffer>
-         autocmd  CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
-         autocmd  CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-         augroup End
-      ]], false)
-   end
-
-   -- }}}
-
-end
 
 -- server configuration {{{1
 
@@ -426,7 +53,9 @@ local configure_servers = function(language_list)
       installer = require('nvim-lsp-installer.servers')
    end
 
-   for _, server in pairs(language_list) do
+   f
+   .iterate(language_list)
+   :foreach(function (_, server)
       local has_settings, settings = pcall(require, 'lsp.' .. server)
 
       if not has_settings then
@@ -434,7 +63,8 @@ local configure_servers = function(language_list)
             'Missing configuration for language server ' .. server,
             '‼ lsp'
          )
-         goto continue
+
+         return
       end
 
       if has_lspinstall then
@@ -445,16 +75,22 @@ local configure_servers = function(language_list)
          end
       end
 
+      local function run_hook (hook, client, bufnr)
+         if typex(hook) == 'function' then
+            hook(client, bufnr, settings)
+         elseif typex(hook) == 'table' then
+            f
+            .iterate(hook)
+            :foreach(function (_, h)
+               h(client, bufnr, settings)
+            end)
+         end
+      end
+
       settings.on_attach = function (client, bufnr)
-         if type(settings.pre_attach) == 'function' then
-            settings.pre_attach(client, bufnr)
-         end
-
-         on_attach(client, bufnr)
-
-         if type(settings.post_attach) == 'function' then
-            settings.post_attach(client, bufnr)
-         end
+         run_hook(settings.pre_attach,  client, bufnr)
+         run_hook(on_attach,            client, bufnr)
+         run_hook(settings.post_attach, client, bufnr)
 
          log.info('Started ' .. server, ' lsp')
       end
@@ -469,9 +105,7 @@ local configure_servers = function(language_list)
 
       -- lspinstall.install_server(language)
       lspconfig[server].setup(settings)
-
-      ::continue::
-   end
+   end)
 end
 
 -- main {{{1
