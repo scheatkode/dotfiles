a39a74bd3bf9c08680c6dadc61c9bb41b1270db4
scheatkode <scheatkode@gmail.com>
2022-02-25 14:37:09 +0100

    :bug: fix(neovim/p): Unpin `lightspeed` plugin after #127 resolution

--- xdg/neovim/lua/plugins/movement/init.lua
+++ xdg/neovim/lua/plugins/movement/init.lua
@@ -8,8 +8,6 @@ return {'ggandor/lightspeed.nvim', opt = true,
       'T',
    },
 
-   commit = '4d8359a30b26ee5316d0e7c79af08b10cb17a57b',
-
    config = function ()
       require('plugins.movement.config')
    end
