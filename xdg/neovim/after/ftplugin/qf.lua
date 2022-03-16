8c5ac87492b022f8a690ef58b25d1e931b71388e
scheatkode <scheatkode@gmail.com>
2022-03-17 00:26:06 +0100

    :sparkles: feat(neovim): Add `quickfix` ftplugin

---
+++ xdg/neovim/after/ftplugin/qf.lua
@@ -0,0 +1,6 @@
+vim.opt_local.buflisted  = false
+vim.opt_local.number     = true
+vim.opt_local.signcolumn = 'no'
+vim.opt_local.bufhidden  = 'wipe'
+
+vim.cmd [[autocmd! BufEnter <buffer> if winnr('$') < 2 | q | endif]]
