773954a393d49aebbad482b8e8ce965c7c75334e
scheatkode <scheatkode@gmail.com>
2022-03-17 00:19:06 +0100

    :arrow_up: fix(neovim/p): Update `friendly-snippets` startup configuration

--- xdg/neovim/lua/plugins/snippets-catalog/init.lua
+++ xdg/neovim/lua/plugins/snippets-catalog/init.lua
@@ -1,6 +1,6 @@
 return {'rafamadriz/friendly-snippets', opt = true,
    after  = 'luasnip',
    config = function ()
-      require('luasnip.loaders.from_vscode').load()
+      require('luasnip.loaders.from_vscode').lazy_load()
    end
 }
