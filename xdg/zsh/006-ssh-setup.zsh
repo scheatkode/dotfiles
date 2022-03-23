d25322c37a8aef138ab4668ec7e7bc23093107ef
scheatkode <scheatkode@gmail.com>
2022-03-23 04:32:07 +0100

    :bug: fix(zsh): Fix lock acquisition mechanism for ssh setup

--- xdg/zsh/006-ssh-setup.zsh
+++ xdg/zsh/006-ssh-setup.zsh
@@ -16,14 +16,33 @@ ssh-add -l > /dev/null 2>&1
 
 if [ "${?}" = '2' ]
 then
+   local lock; lock="${SSH_CONFIG_HOME}/lock"
+
+   # wait for atomically acquired lock
+   while ! (set -C; : >"${lock}") > /dev/null 2>&1
+   do
+      # not waiting to  keep  the  startup  snappy  since  these
+      # operations are fast enough to not require every instance
+      # to `sleep` before checking again.
+      :
+   done
+
+   # if the session file exists, source it and confirm its
+   # validity.
    test -r "${SSH_ENV}" && eval "$(<${SSH_ENV})" > /dev/null
    ssh-add -l > /dev/null 2>&1
 
+   # `ssh-add` returning with an exit status of `2` means it was
+   # unable to contact the `ssh-agent`, in our case, ensuring
+   # the latter was not started. create a new session file.
    if [ "${?}" = '2' ]
    then
       (umask 006; ssh-agent > "${SSH_ENV}")
       eval "$(<${SSH_ENV})" > /dev/null
    fi
+
+   # release the acquired lock.
+   rm "${lock}"
 fi
 
 alias ssh="ssh ${SSH_CONFIG_OPTS}"
