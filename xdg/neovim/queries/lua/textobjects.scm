90d0da7cbec5427b2cc269abbaafd19315fa2a05
scheatkode <scheatkode@gmail.com>
2022-04-14 03:41:31 +0100

    :sparkles: feat(neovim/q): Add Lua treesitter query
    Make identifiers in multiple variable assignment swappable

--- xdg/neovim/queries/lua/textobjects.scm
+++ xdg/neovim/queries/lua/textobjects.scm
@@ -1,3 +1,11 @@
 ((field)        @swappable)
 (arguments  (_) @swappable)
 (parameters (_) @swappable)
+
+;; in multiple variable assignment
+;; ```lua
+;; local has_module, module = pcall(require, 'some.module')
+;; --    ╰────────╯  ╰────╯
+;; --      ╰─ captures ─╯
+;; ```
+(assignment_statement (variable_list name: (identifier) @swappable))
