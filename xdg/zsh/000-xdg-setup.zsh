f4d5c5ddfdf08e3160718eaf81ee0e7816153a1a
scheatkode <scheatkode@gmail.com>
2022-04-14 03:49:51 +0100

    :sparkles: feat(zsh): Add XDG workarounds
    Cargo, rustup, and npm

--- xdg/zsh/000-xdg-setup.zsh
+++ xdg/zsh/000-xdg-setup.zsh
@@ -4,4 +4,9 @@
 #         ░▀░▀░▀▀░░▀▀▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░░
 #
 
+export            CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export           RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 
+export npm_config_userconfig="${XDG_CONFIG_HOME}/npm/npmrc"
+export      npm_config_cache="${XDG_CACHE_HOME}/npm"
+export     npm_config_prefix="${XDG_DATA_HOME}/npm"
