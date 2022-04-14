b90b5b2a0ec8b91f7d5e2ff09a3b04c01f8954a8
scheatkode <scheatkode@gmail.com>
2022-04-14 03:48:37 +0100

    :fire: chore(zsh): Remove redundant xdg setup

--- xdg/zsh/000-xdg-setup.zsh
+++ xdg/zsh/000-xdg-setup.zsh
@@ -4,10 +4,4 @@
 #         ░▀░▀░▀▀░░▀▀▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░░
 #
 
-export   XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
-export  XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 
-[[ -d   "${XDG_DATA_HOME}" ]] || mkdir --parent "${XDG_DATA_HOME}"
-[[ -d  "${XDG_CACHE_HOME}" ]] || mkdir --parent "${XDG_CACHE_HOME}"
-[[ -d "${XDG_CONFIG_HOME}" ]] || mkdir --parent "${XDG_CONFIG_HOME}"
