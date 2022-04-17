b7addf1d63e24e773fa65550654e0351e12b97ab
scheatkode <scheatkode@gmail.com>
2022-04-17 17:07:53 +0100

    feat(neovim): Enable lua `filetype` instead of old vim's

--- xdg/neovim/lua/scheatkode/settings.lua
+++ xdg/neovim/lua/scheatkode/settings.lua
@@ -4,6 +4,9 @@
 --                     ░▀▀▀░▀▀▀░░▀░░░▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀                     --
 ------------------------------------------------------------------------------
 
+vim.g.do_filetype_lua    = 1
+vim.g.did_load_filetypes = 0
+
 local fn  = vim.fn
 local opt = vim.opt
 local o   = vim.o
