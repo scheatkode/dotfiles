576466d0ffc2733e09ce0a5c9cf7db42aa64275a
scheatkode <scheatkode@gmail.com>
2022-04-17 13:31:33 +0100

    refactor(neovim/p): Refactor `comment.nvim` config
    Dump defaults mappings and use the new `<Plug>(*)` mappings instead following
    f9df9847ca403cee7124d862249c02916e773d03

--- xdg/neovim/lua/plugins/comment/config.lua
+++ xdg/neovim/lua/plugins/comment/config.lua
@@ -6,59 +6,38 @@ if not has_comment then
 end
 
 return comment.setup {
-   --- Add a space b/w comment and the line
-   --- @type boolean
+   -- add a space between comment and the line
    padding = true,
 
-   --- Line which should be ignored while comment/uncomment
-   --- Example: Use '^$' to ignore empty lines
-   --- @type string Lua regex
+   -- lines that should be ignored while (un)commenting
+   -- example: use '^$' to ignore empty lines
    ignore = '^$',
 
-   --- Whether to create basic (operator-pending) and extra mappings for
-   --- NORMAL/VISUAL mode
-   --- @type table
+   -- whether to create basic (operator-pending) and extra
+   -- mappings for normal/visual mode
    mappings = {
-      --- operator-pending mapping
-      --- Includes `gcc`, `gcb`, `gc[count]{motion}` and `gb[count]{motion}`
-      basic = true,
-      --- extra mapping
-      --- Includes `gco`, `gcO`, `gcA`
-      extra = true,
-      --- extended mapping
-      --- Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+      -- operator-pending mapping
+      basic = false,
+
+      -- extra mapping
+      extra = false,
+
+      -- extended mapping
       extended = false,
    },
 
-   --- LHS of line and block comment toggle mapping in NORMAL/VISUAL mode
-   --- @type table
-   toggler = {
-      --- line-comment toggle
-      line = 'gcc',
-      --- block-comment toggle
-      block = 'gcb',
-   },
-
-   --- LHS of line and block comment operator-mode mapping in NORMAL/VISUAL mode
-   --- @type table
-   opleader = {
-      --- line-comment opfunc mapping
-      line = 'gc',
-      --- block-comment opfunc mapping
-      block = 'gb',
-   },
-
-   --- Pre-hook, called before commenting the line
-   --- @type function|nil
+   -- pre-hook, called before commenting the line
    pre_hook = function (context)
       local u = require('Comment.utils')
 
-      --- determine whether to use linewise or blockwise commentsring.
+      -- determine whether to use linewise or blockwise
+      -- commentstring
       local type = context.ctype == u.ctype.line
          and '__default'
          or  '__multiline'
 
-      --- determine the location from which to calculate the commentstring.
+      -- determine the location from which to calculate the
+      -- commentstring
       local location = nil
 
       if context.ctype == u.ctype.block then
@@ -73,7 +52,6 @@ return comment.setup {
       })
    end,
 
-   --- Post-hook, called after commenting is done
-   --- @type function|nil
+   -- post-hook, called after commenting is done
    post_hook = nil,
 }
--- xdg/neovim/lua/plugins/comment/keys.lua
+++ xdg/neovim/lua/plugins/comment/keys.lua
@@ -1,6 +1,18 @@
 local function setup ()
-   vim.keymap.set('n', '<leader>/', 'gcc', {remap = true, desc = 'Comment current line'})
-   vim.keymap.set('v', '<leader>/', 'gc',  {remap = true, desc = 'Comment lines'})
+   -- toggle using count
+   vim.keymap.set('n', '<leader>/', 'v:count == 0 ? "<Plug>(comment_toggle_current_linewise)"  : "<Plug>(comment_toggle_linewise_count)"',  {expr = true, remap = true, desc = 'Comment current line(s)'})
+   vim.keymap.set('n', 'gcc',       'v:count == 0 ? "<Plug>(comment_toggle_current_linewise)"  : "<Plug>(comment_toggle_linewise_count)"',  {expr = true, remap = true, desc = 'Comment current line(s)'})
+   vim.keymap.set('n', 'gbc',       'v:count == 0 ? "<Plug>(comment_toggle_current_blockwise)" : "<Plug>(comment_toggle_blockwise_count)"', {expr = true, remap = true, desc = 'Comment current line(s)'})
+
+   -- toggle in op-pending mode
+   vim.keymap.set('n', '<leader>//', '<Plug>(comment_toggle_linewise)',  {remap = true})
+   vim.keymap.set('n', 'gc',         '<Plug>(comment_toggle_linewise)',  {remap = true})
+   vim.keymap.set('n', 'gb',         '<Plug>(comment_toggle_blockwise)', {remap = true})
+
+   -- toggle in visual mode
+   vim.keymap.set('x', '<leader>/', '<Plug>(comment_toggle_linewise_visual)',  {remap = true, desc = 'Comment lines'})
+   vim.keymap.set('x', 'gc',        '<Plug>(comment_toggle_linewise_visual)',  {remap = true, desc = 'Comment lines'})
+   vim.keymap.set('x', 'gb',        '<Plug>(comment_toggle_blockwise_visual)', {remap = true, desc = 'Comment lines'})
 end
 
 return {
