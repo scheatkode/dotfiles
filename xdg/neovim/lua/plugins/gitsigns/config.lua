a64d8377b3f8eef4e9d9ea979a79efaf9ced5dfc
scheatkode <scheatkode@gmail.com>
2022-04-27 17:21:26 +0100

    style(neovim/p): Change gutter git signs

--- xdg/neovim/lua/plugins/gitsigns/config.lua
+++ xdg/neovim/lua/plugins/gitsigns/config.lua
@@ -7,16 +7,11 @@ end
 
 gitsigns.setup({
    signs = {
-      -- add          = {hl = 'DiffAdd',    text = ' █', numhl='GitSignsAddNr',    linehl='GitSignsAddLn'},
-      -- change       = {hl = 'DiffChange', text = ' █', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      -- delete       = {hl = 'DiffDelete', text = ' █', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      -- topdelete    = {hl = 'DiffDelete', text = ' █', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      -- changedelete = {hl = 'DiffChange', text = ' █', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      add          = {hl = 'DiffAdd',    text = ' ', numhl='GitSignsAddNr',    linehl='GitSignsAddLn'},
-      change       = {hl = 'DiffChange', text = ' ', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      delete       = {hl = 'DiffDelete', text = ' ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      topdelete    = {hl = 'DiffDelete', text = ' ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      changedelete = {hl = 'DiffChange', text = ' ', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+      add          = {hl = 'GitSignsAdd',    text = '┃', numhl='GitSignsAddNr',    linehl='GitSignsAddLn'},
+      change       = {hl = 'GitSignsChange', text = '┣', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+      delete       = {hl = 'GitSignsDelete', text = '┻', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+      topdelete    = {hl = 'GitSignsDelete', text = '┳', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+      changedelete = {hl = 'GitSignsChange', text = '╋', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
    },
 
    -- TODO(scheatkode): activate this on v7.0
