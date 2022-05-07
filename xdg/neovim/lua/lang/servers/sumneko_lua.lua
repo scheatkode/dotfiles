7760b9d83aa7be7c9f0b3de93f9f398f450a68c9
scheatkode <scheatkode@gmail.com>
2022-05-07 20:15:22 +0100

    chore(neovim/lsp): Enable hints for `sumneko_lua`

--- xdg/neovim/lua/lang/servers/sumneko_lua.lua
+++ xdg/neovim/lua/lang/servers/sumneko_lua.lua
@@ -79,6 +79,10 @@ return {
             },
          },
 
+         hint = {
+         	enable = true,
+         },
+
          workspace = {
             -- Make the server aware of Neovim runtime files
             -- library = vim.api.nvim_get_runtime_file('', true),
