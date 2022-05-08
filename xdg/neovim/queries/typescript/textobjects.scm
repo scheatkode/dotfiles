6bf21ce894f745d58b0d1315a4efbb0547d98de7
scheatkode <scheatkode@gmail.com>
2022-05-08 18:17:19 +0100

    docs(neovim): Document typescript treesitter captures

--- xdg/neovim/queries/typescript/textobjects.scm
+++ xdg/neovim/queries/typescript/textobjects.scm
@@ -1,3 +1,26 @@
+;; arguments in function call expressions
+;;
+;; ```typescript
+;; func(Math.PI,  Math.E)
+;; //   ╰─────╯   ╰────╯
+;; //    ╰─ captures ─╯
+;; ```
 (arguments (_) @swappable)
+
+;; branches of ternary expressions
+;;
+;; ```typescript
+;; true ? do_something() : do_nothing()
+;; //     ╰────────────╯   ╰──────────╯
+;; //            ╰─ captures ─╯
+;; ```
 (ternary_expression [consequence: (_) alternative: (_)] @swappable)
+
+;; identifiers in named imports
+;;
+;; ```typescript
+;; import { access, ftruncate } from 'fs'
+;; //       ╰────╯  ╰───────╯
+;; //        ╰─ captures ─╯
+;; ```
 (named_imports (import_specifier) @swappable)
