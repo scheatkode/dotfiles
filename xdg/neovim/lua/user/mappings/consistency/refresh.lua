fade0ae2d02b80f0fb31aaf232e27de063cbc5f4
scheatkode <scheatkode@gmail.com>
2022-05-12 04:50:02 +0100

    fix(neovim/keys): Fix refresh keymap

--- xdg/neovim/lua/user/mappings/consistency/refresh.lua
+++ xdg/neovim/lua/user/mappings/consistency/refresh.lua
@@ -1,7 +1,7 @@
 return {
 	---saner `ctrl-l`
 	---https://github.com/mhinz/vim-galore#saner-ctrl-l
-	setup = function ()
-		vim.keymap.set('n', '<C-l>', ':nohlsearch<CR>:diffupdate<CR>')
+	setup = function()
+		vim.keymap.set('n', '<C-l>', ':<C-u>nohlsearch<CR>:diffupdate<CR><C-l>', { silent = true })
 	end
 }
