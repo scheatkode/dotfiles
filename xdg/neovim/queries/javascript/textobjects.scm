42e8bb44babfd8729b262e76f16474bed1a7fcba
scheatkode <scheatkode@gmail.com>
2022-05-26 12:01:06 +0100

    refactor(neovim/query): Refactor to `ts` and `js` queries

---
+++ xdg/neovim/queries/javascript/textobjects.scm
@@ -0,0 +1,47 @@
+;; formal parameters in function declarations
+;;
+;; ```typescript
+;; const func = (x: number,  y = 0) => 2 * x + y
+;; //            ╰───────╯   ╰───╯
+;; //              ╰─ captures ─╯
+;; ```
+(formal_parameters (_) @swappable)
+
+;; arguments in function call expressions
+;;
+;; ```javascript
+;; func(Math.PI,  Math.E)
+;; //   ╰─────╯   ╰────╯
+;; //    ╰─ captures ─╯
+;; ```
+(arguments (_) @swappable)
+
+;; branches of ternary expressions
+;;
+;; ```javascript
+;; true ? do_something() : do_nothing()
+;; //     ╰────────────╯   ╰──────────╯
+;; //            ╰─ captures ─╯
+;; ```
+(ternary_expression [consequence: (_) alternative: (_)] @swappable)
+
+;; properties of objects
+;;
+;; ```javascript
+;; const obj = { property1: 1, property2: 2 }
+;; //            ╰──────────╯  ╰──────────╯
+;; //                 ╰─ captures ─╯
+;; func({ property1, property2 })
+;; //     ╰───────╯  ╰───────╯
+;; //        ╰─ captures ─╯
+;; ```
+(object (_) @swappable)
+
+;; properties of objects
+;;
+;; ```javascript
+;; const arr = [ member1, member2 ]
+;; //            ╰─────╯  ╰─────╯
+;; //             ╰─ captures ─╯
+;; ```
+(array (_) @swappable)
--- xdg/neovim/queries/typescript/textobjects.scm
+++ xdg/neovim/queries/typescript/textobjects.scm
@@ -1,29 +1,4 @@
-;; formal parameters in function declarations
-;;
-;; ```typescript
-;; const func = (x: number,  y = 0) => 2 * x + y
-;; //            ╰───────╯   ╰───╯
-;; //              ╰─ captures ─╯
-;; ```
-(formal_parameters (_) @swappable)
-
-;; arguments in function call expressions
-;;
-;; ```typescript
-;; func(Math.PI,  Math.E)
-;; //   ╰─────╯   ╰────╯
-;; //    ╰─ captures ─╯
-;; ```
-(arguments (_) @swappable)
-
-;; branches of ternary expressions
-;;
-;; ```typescript
-;; true ? do_something() : do_nothing()
-;; //     ╰────────────╯   ╰──────────╯
-;; //            ╰─ captures ─╯
-;; ```
-(ternary_expression [consequence: (_) alternative: (_)] @swappable)
+; inherits javascript
 
 ;; identifiers in named imports
 ;;
@@ -33,18 +8,6 @@
 ;; //        ╰─ captures ─╯
 ;; ```
 (named_imports (import_specifier) @swappable)
-
-;; properties of objects
-;;
-;; ```typescript
-;; const obj = { property1: 1, property2: 2 }
-;; //            ╰──────────╯  ╰──────────╯
-;; //                 ╰─ captures ─╯
-;; func({ property1, property2 })
-;; //     ╰───────╯  ╰───────╯
-;; //        ╰─ captures ─╯
-;; ```
-(object (_) @swappable)
 
 ;; type arguments
 ;;
