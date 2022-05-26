7c455155c03953c9603b2e22ae2425d3eec15d6f
scheatkode <scheatkode@gmail.com>
2022-05-26 11:59:50 +0100

    feat(zsh/path): Add `pnpm`'s home to `$PATH`

--- xdg/zsh/010-path-setup.zsh
+++ xdg/zsh/010-path-setup.zsh
@@ -4,4 +4,5 @@
 #       ░▀░░░▀░▀░░▀░░▀░▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀░░
 #
 
-export PATH="${XDG_DATA_HOME}/npm/bin:/usr/local/bin:${PATH}"
+export PNPM_HOME="${XDG_DATA_HOME}/pnpm"
+export PATH="${PNPM_HOME}:${XDG_DATA_HOME}/npm/bin:/usr/local/bin:${PATH}"
