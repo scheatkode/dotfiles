c15b76ec58d1c9cffc6a5570e4163f4a613de9d9
scheatkode <scheatkode@gmail.com>
2022-05-31 11:19:27 +0100

    feat(neovim/mappings): Add shortcut for Ex-mode

--- xdg/neovim/lua/user/mappings/custom/command.lua
+++ xdg/neovim/lua/user/mappings/custom/command.lua
@@ -2,6 +2,7 @@ return {
 	---`;` is faster than `:` to run commands. win a keystroke,
 	---profit.
 	setup = function()
-		vim.keymap.set({ 'n', 'x' }, ';', ':', { nowait = true })
+		vim.keymap.set({ 'n', 'x' }, ';',  ':',  { nowait = true })
+		vim.keymap.set({ 'n', 'x' }, 'q;', 'q:', { nowait = true })
 	end
 }
