7eef9cf1aaf8d309d7efb75685b1138755df66b0
scheatkode <scheatkode@gmail.com>
2022-05-31 11:18:55 +0100

    style(neovim): Minor code formatting and cleanup

--- xdg/neovim/lua/user/mappings/custom/escape.lua
+++ xdg/neovim/lua/user/mappings/custom/escape.lua
@@ -3,7 +3,7 @@ return {
 	setup = function()
 		vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
-		vim.keymap.set({ 'i', 'v', 'c', 'o' }, 'jk', '<Esc>')
+		vim.keymap.set({ 'i', 'v', 'c', 'o' }, 'jk',    '<Esc>')
 		vim.keymap.set({ 'i', 'v', 'c', 'o' }, '<C-c>', '<Esc>')
 	end
 }
--- xdg/neovim/lua/user/mappings/custom/highlight.lua
+++ xdg/neovim/lua/user/mappings/custom/highlight.lua
@@ -1,7 +1,6 @@
 return {
 	---remove search highlight on `<Esc>`
 	setup = function()
-   	-- vim.keymap.set('n', '<Esc>', '<cmd>let @/=""<CR>', {remap = true, silent = true})
 		vim.keymap.set('n', '<Esc>', '<cmd>:nohlsearch<CR>', { remap = true, silent = true })
 	end
 }
