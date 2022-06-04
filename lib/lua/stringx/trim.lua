05dc37365bb83fb9720f1f7abfb42f7c86e78b7d
scheatkode <scheatkode@gmail.com>
2022-06-04 22:38:31 +0100

    feat(lib/stringx): Add `trim` to new `stringx` lib

---
+++ lib/lua/stringx/trim.lua
@@ -0,0 +1,16 @@
+---Return a copy of the string with the leading and trailing characters
+---removed.
+---@param s string
+---@return string s Trimmed string.
+local function trim(s)
+	-- The '()' avoids the overhead of default string capture. This overhead is
+	-- small, ~10% for successful whitespace match call alone, and may not be
+	-- noticeable even in benchmarks, but there's little harm especially for
+	-- such a simple, basic, and commonly used function; every performance
+	-- squeeze is beneficiary.
+	local from = s:match('^%s*()')
+
+	return from > #s and '' or s:match('.*%S', from)
+end
+
+return trim
---
+++ lib/lua/stringx/trim_spec.lua
@@ -0,0 +1,41 @@
+local trim = require('stringx.trim')
+
+describe('stringx', function()
+	describe('trim()', function()
+		it('should do nothing when given a trimmed string', function()
+			assert.are.equal('abc', trim('abc'))
+		end)
+
+		it('should trim the left side of the given string', function()
+			assert.are.equal('abc', trim('   abc'))
+		end)
+
+		it('should trim the right side of the given string', function()
+			assert.are.equal('abc', trim('abc   '))
+		end)
+
+		it('should trim both sides of the given string', function()
+			assert.are.equal('abc', trim('   abc   '))
+		end)
+
+		it('should trim newlines', function()
+			assert.are.equal('abc', trim([[
+				abc
+			]]))
+		end)
+
+		it('should trim newlines and carriage returns', function()
+			assert.are.equal('abc', trim([[
+				abc
+			]]))
+		end)
+
+		it('should trim tabs', function()
+			assert.are.equal('abc', trim([[	abc	]]))
+		end)
+
+		it('should not trim no-break spaces', function()
+			assert.are_not.equal('abc', trim([[ abc ]]))
+		end)
+	end)
+end)
