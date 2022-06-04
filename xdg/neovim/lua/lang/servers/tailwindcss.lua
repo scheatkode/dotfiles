5753923f28d5ee0d583103d7a4375baf78b99e35
scheatkode <scheatkode@gmail.com>
2022-06-04 13:54:11 +0100

    refactor(neovim/lsp): Rewrite `tailwindcss` config

--- xdg/neovim/lua/lang/servers/tailwindcss.lua
+++ xdg/neovim/lua/lang/servers/tailwindcss.lua
@@ -1,17 +1,18 @@
 return {
-   cmd = {
-      vim.fn.expand(table.concat({'~',
-         '.local',
-         'share',
-         'nvim',
-         'lsp_servers',
-         'tailwindcss_npm',
-         'node_modules',
-         -- '@tailwindcss',
-         -- 'language-server',
-         '.bin',
-         'tailwindcss-language-server',
-      }, '/')),
-      '--stdio',
-   },
+	-- This server gets spawned all over the place while being useless most of
+	-- the time. Starting it manually so I don't have to manage 15 running
+	-- servers at once.
+	autostart = false,
+
+	cmd = {
+		vim.fn.expand(table.concat({
+			vim.fn.stdpath('data'),
+			'lsp_servers',
+			'tailwindcss_npm',
+			'node_modules',
+			'.bin',
+			'tailwindcss-language-server',
+		}, '/')),
+		'--stdio',
+	},
 }
