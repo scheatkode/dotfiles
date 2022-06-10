c9f46db59f840471a1efba89208841b42f4f6ee3
scheatkode <scheatkode@gmail.com>
2022-06-10 02:01:16 +0100

    feat(neovim/ts): Add query for `svelte`

---
+++ xdg/neovim/queries/svelte/textobjects.scm
@@ -0,0 +1,9 @@
+;; element attributes
+;;
+;; ```html
+;; <div class="some-class" style="some: style;" />
+;; //   ╰────────────────╯ ╰──────────────────╯
+;; //              ╰─ captures ─╯
+;; ```
+(element (_ (attribute) @swappable))
+
