a31694d16eb77307b90e0b095d5ba0387353e2a7
scheatkode <scheatkode@gmail.com>
2022-06-13 14:38:27 +0100

    refactor(lib/compat): Extract `package.searchpath`

---
+++ lib/lua/compat/searchpath/init.lua
@@ -0,0 +1,63 @@
+if package.searchpath then
+	return package.searchpath
+end
+
+local assertx   = require('assertx')
+local separator = require('compat.separator')
+
+---Get the full path where a file name would have matched. This
+---function was introduced in Lua 5.2. This compatibility version
+---will be injected in Lua 5.1 engines.
+---
+---@param name string file name, possibly dotted
+---@param path string a path-template in the same form as `package.path` or `package.cpath`
+---@param sep? string template separate character to be replaced by path separator. Default: '.'
+---@param rep? string path separator to use, defaults to system separator
+---@return string? filename path of the file on success
+---@return string? errormsg on failure, the error string lists the paths tried
+return function(name, path, sep, rep)
+	local type = type
+
+	assertx(type(name) == 'string', string.format, 'bad argument #1 to \'searchpath\' (string expected, got %s)', type(path), 2)
+	assertx(type(path) == 'string', string.format, 'bad argument #2 to \'searchpath\' (string expected, got %s)', type(path), 2)
+	assertx(sep == nil or type(sep) == 'string', string.format, 'bad argument #3 to \'searchpath\' (string expected, got %s)', type(path), 2)
+	assertx(rep == nil or type(rep) == 'string', string.format, 'bad argument #4 to \'searchpath\' (string expected, got %s)', type(path), 2)
+
+	sep = sep or '.'
+	rep = rep or separator
+
+	do
+		local s, e = name:find(sep, nil, true)
+
+		while s do
+			name = name:sub(1, s - 1)
+				 .. rep
+				 .. name:sub(e + 1, -1)
+
+			s, e = name:find(
+				sep,
+				s + #rep + 1,
+				true
+			)
+		end
+	end
+
+	local paths_tried = {}
+
+	for m in path:gmatch('[^;]+') do
+		local nm = m:gsub('?', name)
+		local f  = io.open(nm, 'r')
+
+		paths_tried[#paths_tried + 1] = nm
+
+		if f then
+			f:close()
+			return nm
+		end
+	end
+
+	return nil,
+		 '\n\tno file \''
+		 .. table.concat(paths_tried, '\'\n\tno file \'')
+		 .. '\''
+end
---
+++ lib/lua/compat/searchpath/init_spec.lua
@@ -0,0 +1,46 @@
+local searchpath = require('compat.searchpath')
+
+describe('compat', function()
+	describe('package.searchpath()', function()
+
+		local original_searchpath = package.searchpath
+
+		before_each(function()
+			package.searchpath = nil
+			package.loaded['compat.searchpath'] = nil
+			package.searchpath = require('compat.searchpath')
+		end)
+
+		after_each(function() package.searchpath = original_searchpath end)
+
+		it('should match the original function behaviour when a package is found', function()
+			local path = 'lib/lua/?/init.lua'
+
+			assert.is_truthy(
+				searchpath('compat', path):match('lib[/\\]lua[/\\]compat')
+			)
+		end)
+
+		local path = 'some/?/random.path;another/?.path'
+
+		it('should match the original function behaviour when a package is not found', function()
+			local ok, err = searchpath('some.file.name', path, '.', '/')
+
+			assert.is_nil(ok)
+			assert.same(
+				'\n\tno file \'some/some/file/name/random.path\'\n\tno file \'another/some/file/name.path\'',
+				err
+			)
+		end)
+
+		it('should match the original function behaviour when a package is not found', function()
+			local ok, err = searchpath('some/file/name', path, '/', '.')
+
+			assert.is_nil(ok)
+			assert.same(
+				'\n\tno file \'some/some.file.name/random.path\'\n\tno file \'another/some.file.name.path\'',
+				err
+			)
+		end)
+	end)
+end)
