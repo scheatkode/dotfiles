8a16f262c968d8d9027434122035f99c23f1adb3
scheatkode <scheatkode@gmail.com>
2022-07-04 15:13:36 +0100

    feat(zsh): Add stopwatch

--- xdg/zsh/900-functions.zsh
+++ xdg/zsh/900-functions.zsh
@@ -126,3 +126,14 @@ split("░░░░x░▀▀▀x░░░░",   font["-"], "x")
 function calc () {
    printf "%s\n" "${@}" | bc -l
 }
+
+function stopwatch () {
+	local start; start="$(date '+%s')"
+
+	while true;
+	do
+		time="$(( $(date '+%s') - start))"
+		printf '%s\r' "$(date -u -d "@${time}" '+%H:%M:%S')"
+		sleep 0.2
+	done
+}
