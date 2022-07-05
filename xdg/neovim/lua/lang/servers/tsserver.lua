58021818a5b034fb8417c63aa790163778e75248
scheatkode <scheatkode@gmail.com>
2022-07-05 14:17:50 +0100

    style(neovim/lsp): Format & fix tsserver config

--- xdg/neovim/lua/lang/servers/tsserver.lua
+++ xdg/neovim/lua/lang/servers/tsserver.lua
@@ -1,41 +1,39 @@
 local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
 
 if not has_lspconfig then
-   print('‼ Tried loading lspconfig for tsserver ... unsuccessfully.')
-   return has_lspconfig
+	print('‼ Tried loading lspconfig for tsserver ... unsuccessfully.')
+	return has_lspconfig
 end
 
 -- TODO(scheatkode): Add autoinstall with spinner animation
 
 return {
-   cmd = {
-      vim.fn.expand(table.concat({'~',
-         '.local',
-         'share',
-         'nvim',
-         'lsp_servers',
-         'tsserver',
-         'node_modules',
-         'typescript-language-server',
-         'lib',
-         'cli.js',
-      }, '/')),
-      '--stdio'
-   },
+	cmd = {
+		vim.fn.expand(table.concat({
+			vim.fn.stdpath('data'),
+			'lsp_servers',
+			'tsserver',
+			'node_modules',
+			'typescript-language-server',
+			'lib',
+			'cli.js',
+		}, '/')),
+		'--stdio'
+	},
 
-   filetypes = {
-      'javascript',
-      'javascriptreact',
-      'javascript.jsx',
-      'typescript',
-      'typescriptreact',
-      'typescript.tsx',
-   },
+	filetypes = {
+		'javascript',
+		'javascriptreact',
+		'javascript.jsx',
+		'typescript',
+		'typescriptreact',
+		'typescript.tsx',
+	},
 
-   root_dir = lspconfig.util.root_pattern(
-      "package.json",
-      "tsconfig.json",
-      "jsconfig.json",
-      ".git"
-   )
+	root_dir = lspconfig.util.root_pattern(
+		'package.json',
+		'tsconfig.json',
+		'jsconfig.json',
+		'.git'
+	)
 }
