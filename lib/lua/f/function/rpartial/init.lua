c3463a53d1cb0c724df848bc9c20724e0c824955
scheatkode <scheatkode@gmail.com>
2022-07-05 12:58:28 +0100

    feat(lib/f): Add right partial application

---
+++ lib/lua/f/function/rpartial/init.lua
@@ -0,0 +1,47 @@
+local pack   = require('compat.table.pack')
+local unpack = require('compat.table.unpack')
+
+---A function that partially applies given arguments to the
+---right of function `f`, producing another function with
+---smaller arity.
+---
+---Given a function `f: (X × Y × Z) → N`, we might bind the
+---first argument, producing a function of type
+---`rpartial(f): (X × Y) → N`.
+---
+---```lua
+---local rpartial = require('f.function.rpartial')
+---local pipe     = require('f.function.pipe')
+---
+---local add = function (x, y) return x + y end
+---local mul = function (x, y) return x * y end
+---
+---local add2 = rpartial(add, 2)
+---local mul3 = rpartial(mul, 3)
+---
+---assert.are.same(9, pipe(1, add2, mul3))
+---```
+---
+---@param f function
+---@vararg any
+---@return function
+local function rpartial(f, ...)
+	local params   = pack(...)
+	local paramlen = select('#', ...)
+
+	return function(...)
+		local t = pack(...)
+
+		for i = 1, paramlen do
+			t[#t + 1] = params[i]
+		end
+
+		if t.n ~= nil then
+			t.n = t.n + paramlen
+		end
+
+		return f(unpack(t))
+	end
+end
+
+return rpartial
---
+++ lib/lua/f/function/rpartial/init_spec.lua
@@ -0,0 +1,58 @@
+local fmt = string.format
+
+local f        = require('f')
+local rpartial = require('f.function.rpartial')
+local pipe     = require('f.function.pipe')
+
+describe('function', function()
+	describe('rpartial', function()
+		local add = function(x, y) return x + y end
+		local mul = function(x, y) return x * y end
+
+		f.zip(
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50)
+		)
+			:foreach(function(adder, multiplier, number)
+				it(fmt('should partially apply through the pipe such that (%3d + %3d) * %3d', number, adder, multiplier), function()
+					assert.are.same(
+						(number + adder) * multiplier,
+						pipe(number, rpartial(add, adder), rpartial(mul, multiplier))
+					)
+				end)
+			end)
+
+		local discriminant = function(a, b, c)
+			return b * b - 4 * a * c
+		end
+
+		f.zip(
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50)
+		)
+			:foreach(function(a, b, c)
+				it(fmt('should partially apply the function with one argument (%3d, %3d, %3d)', a, b, c), function()
+					assert.are.same(
+						c * c - 4 * b * a,
+						rpartial(discriminant, a)(b, c)
+					)
+				end)
+			end)
+
+		f.zip(
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50),
+			f.random(0, 1000):take(50)
+		)
+			:foreach(function(a, b, c)
+				it(fmt('should partially apply the function with multiple arguments (%3d, %3d, %3d)', a, b, c), function()
+					assert.are.same(
+						a * a - 4 * c * b,
+						pipe(c, rpartial(discriminant, a, b))
+					)
+				end)
+			end)
+	end)
+end)
