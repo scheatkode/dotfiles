b88382eb24bbe0b18a3664d8c9acba7d818f610e
scheatkode <scheatkode@gmail.com>
2022-07-23 13:34:48 +0100

    style(lib/f): Minor tweaks to code style

--- lib/lua/f/function/flip/init.lua
+++ lib/lua/f/function/flip/init.lua
@@ -1,14 +1,16 @@
---- flip the order of a binary function's arguments.
+local curry = require('f.function.curry')
+
+---flip the order of a binary function's arguments.
 ---
---- @generic A
---- @generic B
---- @generic C
+---@generic A
+---@generic B
+---@generic C
 ---
---- @type fun(f: fun(a: A, b: B): C): fun(b: B, a: A): C
+---@type fun(f: fun(a: A, b: B): C): fun(b: B, a: A): C
 ---
---- @param f function function to apply
---- @return function f a function accepting the flipped arguments
-local function flip (f)
+---@param f function function to apply
+---@return function f a function accepting the flipped arguments
+local function flip(f)
    return function(b, a) return f(a, b) end
 end
 
