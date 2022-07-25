0753ebbeea8460eb66dd9fa12f14279ec99e3fe9
scheatkode <scheatkode@gmail.com>
2022-07-25 23:38:16 +0100

    fix(neovim/p): Fix `jsonls` integration with `schemastore`

--- xdg/neovim/lua/lang/servers/jsonls.lua
+++ xdg/neovim/lua/lang/servers/jsonls.lua
@@ -1,8 +1,5 @@
 local has_lspconfig, lspconfig = pcall(require, 'lspconfig')
 
-local constant = require('f.function.constant')
-local ternary  = require('f.function.ternary')
-
 if not has_lspconfig then
 	print('‼ Tried loading lspconfig for jsonls ... unsuccessfully.')
 	return has_lspconfig
@@ -12,9 +9,11 @@ end
 
 local has_schemastore, schemastore = pcall(require, 'schemastore')
 
-if not has_schemastore then
-	print('! Schemastore not found, autocompletion will be unavailable.')
-end
+local settings = has_schemastore and {
+	json     = { schemas = schemastore.json.schemas() },
+	validate = true
+} or nil
+
 
 return {
 	filetypes = {
@@ -23,10 +22,5 @@ return {
 	},
 
 	root_dir = lspconfig.util.root_pattern('.git', vim.fn.getcwd()),
-
-	settings = ternary(
-		has_schemastore,
-		constant({ json = { schemas = schemastore.json.schemas() } }),
-		constant(nil)
-	)
+	settings = settings
 }
