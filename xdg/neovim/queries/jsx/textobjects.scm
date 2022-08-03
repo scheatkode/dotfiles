abb490818c1a08920e831f3cef6f8ec700c4e445
scheatkode <scheatkode@gmail.com>
2022-08-03 19:21:22 +0100

    feat(neovim/q): Add `(j|t)sx` property capture

---
+++ xdg/neovim/queries/jsx/textobjects.scm
@@ -0,0 +1,9 @@
+; inherits: javascript
+
+;; Attributes in JSX.
+;; ```jsx
+;; <div classname='w-full h-full' id='div-1' />
+;; //   ╰───────────────────────╯ ╰────────╯
+;; //                    ╰─ captures ─╯
+;; ```
+(jsx_attribute) @swappable
