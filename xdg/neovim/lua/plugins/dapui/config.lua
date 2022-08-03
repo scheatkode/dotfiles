412a12e64071d2add033b6eb6f3298dfd4222e46
scheatkode <scheatkode@gmail.com>
2022-08-03 19:36:39 +0100

    fix(neovim/p): Fix deprecations for `dap-ui`

--- xdg/neovim/lua/plugins/dapui/config.lua
+++ xdg/neovim/lua/plugins/dapui/config.lua
@@ -8,57 +8,57 @@ local log = require('log')
 local has_dapui, dapui = pcall(require, 'dapui')
 
 if not has_dapui then
-   log.error('Tried loading plugin ... unsuccessfully ‼', 'dap-ui')
-   return has_dapui
+	log.error('Tried loading plugin ... unsuccessfully ‼', 'dap-ui')
+	return has_dapui
 end
 
 -- }}}
 -- configure plugin {{{
 
 dapui.setup {
-   icons = {
-      expanded  = '▾',
-      collapsed = '▸'
-   },
+	icons = {
+		expanded  = '▾',
+		collapsed = '▸'
+	},
 
-   mappings = {
-      expand = { '<Tab>' },
-      open   = { 'o', '<CR>' },
-      remove = 'd',
-      edit   = 'e',
-      repl   = 'r',
-      toggle = 't',
-   },
+	mappings = {
+		expand = { '<Tab>' },
+		open   = { 'o', '<CR>' },
+		remove = 'd',
+		edit   = 'e',
+		repl   = 'r',
+		toggle = 't',
+	},
 
-   sidebar = {
-      elements      = {
-         {id = 'scopes',      size = 0.25},
-         {id = 'breakpoints', size = 0.25},
-         {id = 'stacks',      size = 0.25},
-         {id = 'watches',     size = 0.25},
-      },
-      position = 'left',
-      size     = 40,
-   },
+	layouts = {
+		{
+			elements = {
+				{ id = 'scopes', size = 0.25 },
+				{ id = 'breakpoints', size = 0.25 },
+				{ id = 'stacks', size = 0.25 },
+				{ id = 'watches', size = 0.25 },
+			},
+			position = 'left',
+			size     = 40,
+		}, {
+			elements = { 'repl' },
+			position = 'bottom',
+			size     = 10,
+		},
+	},
 
-   tray = {
-      elements      = { 'repl' },
-      position = 'bottom',
-      size     = 10,
-   },
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width  = nil, -- Floats will be treated as percentage of your screen.
+		border     = 'rounded',
+		mappings   = {
+			close = { 'q', '<Esc>' },
+		}
+	},
 
-   floating = {
-      max_height = nil, -- These can be integers or a float between 0 and 1.
-      max_width  = nil,  -- Floats will be treated as percentage of your screen.
-      border     = 'rounded',
-      mappings   = {
-      	close = {'q', '<Esc>'},
-      }
-   },
+	windows = { indent = 1 },
 
-   windows = {indent = 1},
-
-   render = {max_type_length = nil},
+	render = { max_type_length = nil },
 }
 
 -- }}}
