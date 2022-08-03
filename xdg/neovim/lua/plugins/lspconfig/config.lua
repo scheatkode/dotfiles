823705fa0e4d6deaf78837b217c5346b6bd584c6
scheatkode <scheatkode@gmail.com>
2022-08-03 19:30:46 +0100

    style(neovim/p): Format code

--- xdg/neovim/lua/plugins/lspconfig/config.lua
+++ xdg/neovim/lua/plugins/lspconfig/config.lua
@@ -13,100 +13,86 @@ local log = require('log')
 
 local text_change_default_debounce = 150
 local languages = {
-   powershell = 'powershell_es',
-   python = 'pyright',
-   -- 'gopls',
-   rust =  'rust_analyzer',
-   javascript = 'tsserver',
-   -- 'vimls',
-   bash   = 'bashls',
-   cpp    = 'ccls',
-   -- deno   = 'denols',
-   php    = 'intelephense',
-   java   = 'jdtls',
-   dart = 'dartls',
-   json   = 'jsonls',
-   -- csharp = 'csharp_ls',
-   csharp = 'omnisharp',
-   -- 'perlls',
-   -- 'perlpls',
-   lua    = 'sumneko_lua',
-   -- 'phpactor',
-   yaml   = 'yamlls',
-   cmake = 'cmake',
-
-   go = 'gopls',
-
-   eslint     = 'eslint',
-   tailwindcss = 'tailwindcss',
-   svelte = 'svelte',
-	css = 'cssls',
-	prisma = 'prismals',
-
-   salt_ls = 'salt_ls',
-   ltex = 'ltex',
-   html = 'html',
-
-   texlab = 'texlab'
+	bash        = 'bashls',
+	cmake       = 'cmake',
+	cpp         = 'ccls',
+	csharp      = 'omnisharp',
+	css         = 'cssls',
+	dart        = 'dartls',
+	eslint      = 'eslint',
+	go          = 'gopls',
+	html        = 'html',
+	java        = 'jdtls',
+	javascript  = 'tsserver',
+	json        = 'jsonls',
+	ltex        = 'ltex',
+	lua         = 'sumneko_lua',
+	php         = 'intelephense',
+	powershell  = 'powershell_es',
+	prisma      = 'prismals',
+	python      = 'pyright',
+	rust        = 'rust_analyzer',
+	salt_ls     = 'salt_ls',
+	svelte      = 'svelte',
+	tailwindcss = 'tailwindcss',
+	texlab      = 'texlab',
+	yaml        = 'yamlls',
 }
 
 -- server configuration {{{1
 
 local configure_servers = function(language_list)
-   local capabilities = require('lang.capabilities')
+	local capabilities = require('lang.capabilities')
 
-   f
-   .iterate(language_list)
-   :foreach(function (_, server)
-      local has_settings, settings = pcall(require, 'lang.servers.' .. server)
+	f
+		 .iterate(language_list)
+		 :foreach(function( _, server)
+		    local has_settings, settings = pcall(require, 'lang.servers.' .. server)
 
-      if not has_settings then
-         log.error(
-            'Missing configuration for language server ' .. server,
-            '‼ lsp'
-         )
+		    if not has_settings then
+		       log.error(
+		          'Missing configuration for language server ' .. server,
+		          '‼ lsp'
+		       )
 
-         return
-      end
+		       return
+		    end
 
-      local function run_hook (hook, client, bufnr, s)
-         if typex(hook) == 'function' then
-            hook(client, bufnr, s)
-         elseif typex(hook) == 'table' then
-            f
-            .iterate(hook)
-            :foreach(function (_, h)
-               h(client, bufnr, s)
-            end)
-         end
-      end
+		    local function run_hook(    hook, client, bufnr, s)
+		       if typex(hook) == 'function' then
+		          hook(client, bufnr, s)
+		       elseif typex(hook) == 'table' then
+		          f
+			           .iterate(hook)
+			           :foreach(function(           _, h)
+			              h(client, bufnr, s)
+			           end)
+		       end
+		    end
 
-      settings.on_attach = function (client, bufnr)
-         run_hook(settings.pre_attach,  client, bufnr, settings)
-         run_hook(on_attach,            client, bufnr, settings)
-         run_hook(settings.post_attach, client, bufnr, settings)
+		    settings.on_attach = function(    client, bufnr)
+		       run_hook(settings.pre_attach, client, bufnr, settings)
+		       run_hook(on_attach, client, bufnr, settings)
+		       run_hook(settings.post_attach, client, bufnr, settings)
 
-         log.info('Started ' .. server, ' lsp')
-      end
+		       log.info('Started ' .. server, ' lsp')
+		    end
 
-      settings.flags = {
-         debounce_text_changes = text_change_default_debounce,
-      }
+		    settings.flags = {
+		       debounce_text_changes = text_change_default_debounce,
+		    }
 
-      settings.capabilities = capabilities
+		    settings.capabilities = capabilities
 
-      -- TODO: refactor and use lspinstall's post install hook
-
-      -- lspinstall.install_server(language)
-      lspconfig[server].setup(settings)
-   end)
+		    lspconfig[server].setup(settings)
+		 end)
 end
 
 -- main {{{1
 
 if not has_lspconfig then
-   log.error('Tried loading plugin ... unsuccessfully', '‼ lsp')
-   return has_lspconfig
+	log.error('Tried loading plugin ... unsuccessfully', '‼ lsp')
+	return has_lspconfig
 end
 
 require('lang.protocol').setup()
@@ -119,4 +105,3 @@ log.info('Plugin loaded', ' lsp')
 return true
 
 -- vim: set fdm=marker fdl=0:
-
