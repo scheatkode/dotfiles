a6533d87f421cdf0cfe7703b3c445721cf798b6e
scheatkode <scheatkode@gmail.com>
2022-08-13 03:46:34 +0100

    feat(neovim/p): Add devicons
    Devicons added for filetypes:
       - yaml
       - sls
       - jinja

--- xdg/neovim/lua/plugins/devicons/init.lua
+++ xdg/neovim/lua/plugins/devicons/init.lua
@@ -1,6 +1,33 @@
-return {'kyazdani42/nvim-web-devicons', opt = true,
-   module = 'nvim-web-devicons',
-   config = function ()
-      require('nvim-web-devicons').setup({ default = true })
-   end,
+return { 'kyazdani42/nvim-web-devicons', opt = true,
+	module = 'nvim-web-devicons',
+	config = function()
+		require('nvim-web-devicons').setup({
+			default = true,
+
+			override = {
+				jinja = {
+					-- close enough.
+					icon        = '關',
+					color       = '#c00000',
+					cterm_color = '9',
+					name        = 'Jinja2',
+				},
+
+				sls = {
+					-- close enough.
+					icon        = '◳',
+					color       = '#dddddd',
+					cterm_color = '15',
+					name        = 'Salt',
+				},
+
+				yaml = {
+					icon        = '!',
+					color       = '#a074c4',
+					cterm_color = '140',
+					name        = 'Yaml',
+				},
+			}
+		})
+	end,
 }
