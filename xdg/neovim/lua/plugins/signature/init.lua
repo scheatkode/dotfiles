c03a0e8c7f45a45da352c717e156a86c63f344a2
scheatkode <scheatkode@gmail.com>
2022-08-23 00:21:40 +0100

    style(neovim/p): Format code

--- xdg/neovim/lua/plugins/signature/init.lua
+++ xdg/neovim/lua/plugins/signature/init.lua
@@ -1,9 +1,14 @@
-return {'ray-x/lsp_signature.nvim', opt = true,
-   event    = { 'InsertEnter'           },
-   module   = { 'lsp_signature'         },
-   wants    = { 'nvim-lspconfig'        },
-   requires = { 'neovim/nvim-lspconfig' },
-   config   = function ()
-      require('plugins.signature.config')
-   end,
+return {
+	'ray-x/lsp_signature.nvim',
+
+	opt    = true,
+	event  = { 'InsertEnter' },
+	module = { 'lsp_signature' },
+
+	wants    = { 'nvim-lspconfig' },
+	requires = { 'neovim/nvim-lspconfig' },
+
+	config = function()
+		require('plugins.signature.config')
+	end,
 }
