8bc7a5d2fa066b9234638cb88b0fb19af928b0da
scheatkode <scheatkode@gmail.com>
2022-08-24 17:11:04 +0100

    refactor(neovim/u): Trade start time for memory consumption

--- xdg/neovim/lua/user/providers/init.lua
+++ xdg/neovim/lua/user/providers/init.lua
@@ -1,22 +1,17 @@
-local tablex = require('tablex')
-
-local defaults = {
-	loaded_node_provider    = 0,
-	loaded_perl_provider    = 0,
-	loaded_python_provider  = 0,
-	loaded_python3_provider = 0,
-	loaded_ruby_provider    = 0,
-}
-
----Disable neovim providers. By default, disables all providers.
----@param overrides? table providers to keep.
 return {
+	---Disable neovim providers. By default, disables all providers.
+	---@param overrides? table providers to keep.
 	disable = function(overrides)
-		local providers = tablex.deep_extend(
-			'force',
-			defaults,
-			overrides or {}
-		)
+		local defaults = {
+			loaded_node_provider    = 0,
+			loaded_perl_provider    = 0,
+			loaded_python_provider  = 0,
+			loaded_python3_provider = 0,
+			loaded_ruby_provider    = 0,
+		}
+
+		local extend    = require('tablex').deep_extend
+		local providers = extend('force', defaults, overrides or {})
 
 		for k, v in pairs(providers) do
 			vim.g[k] = v
