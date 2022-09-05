6d465f098f05a360157b21073e676b3e5cc991aa
scheatkode <scheatkode@gmail.com>
2022-09-05 14:55:26 +0100

    fix(neovim/u): Make command line case insensitive

--- xdg/neovim/lua/user/settings/search/init.lua
+++ xdg/neovim/lua/user/settings/search/init.lua
@@ -9,5 +9,22 @@ return {
 		vim.opt.regexpengine = 0 -- Set regexp engine to auto
 		vim.opt.wrapscan     = true -- Searches wrap around the end of the file
 		vim.opt.inccommand   = 'split' -- Preview substitute results
+
+		-- Dynamically disable `smartcase` in command mode complete while
+		-- ignoring case.
+		local augroup = vim.api.nvim_create_augroup('DynamicSmartcase', { clear = true })
+
+		vim.api.nvim_create_autocmd('CmdLineEnter', {
+			group = augroup,
+			pattern = ':',
+			callback = function() vim.opt.smartcase = false end,
+		})
+
+
+		vim.api.nvim_create_autocmd('CmdLineLeave', {
+			group = augroup,
+			pattern = ':',
+			callback = function() vim.opt.smartcase = true end,
+		})
 	end
 }
