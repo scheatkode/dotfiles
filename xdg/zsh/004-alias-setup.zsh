0e9efa8e58514d32898e0b84816c3413aca12bf5
scheatkode <scheatkode@gmail.com>
2022-09-06 20:56:12 +0100

    feat(zsh): Add alias to continue rebase without edit

--- xdg/zsh/004-alias-setup.zsh
+++ xdg/zsh/004-alias-setup.zsh
@@ -9,3 +9,4 @@ alias 'v'='nvim'
 
 alias 'gc!!'='git commit --amend --no-edit'
 alias 'ga!'='git ls-files -m -o --exclude-standard | fzf -m --print0 | xargs -0 -o -t git add'
+alias 'grbc!'='GIT_EDITOR=true grbc'
