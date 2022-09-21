37f64ff5703b385b8ce804c60ef29e846841c3f0
scheatkode <scheatkode@gmail.com>
2022-09-21 20:06:22 +0100

    feat(neovim/ft): Add cancel keybing to `gitcommit`

--- xdg/neovim/after/ftplugin/gitcommit.lua
+++ xdg/neovim/after/ftplugin/gitcommit.lua
@@ -1,1 +1,2 @@
-vim.keymap.set({'n', 'i'}, '<C-c><C-c>', '<cmd>wq<CR>', { buffer = true })
+vim.keymap.set({ 'n', 'i' }, '<C-c><C-c>', '<cmd>wq<CR>', { buffer = true })
+vim.keymap.set({ 'n', 'i' }, '<C-c><C-k>', '<cmd>q!<CR>', { buffer = true })
