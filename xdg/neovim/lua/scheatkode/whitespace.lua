a5a054b22c679f5595eda2a908340eefa3ae0825
scheatkode <scheatkode@gmail.com>
2022-09-30 21:40:59 +0100

    fix(neovim/u): Fix deprecations
    diff --git a/xdg/neovim/lua/scheatkode/whitespace.lua b/xdg/neovim/lua/scheatkode/whitespace.lua
    index 8417381..159fe50 100644
    --- a/xdg/neovim/lua/scheatkode/whitespace.lua
    +++ b/xdg/neovim/lua/scheatkode/whitespace.lua
    @@ -1,54 +1,55 @@
     local h = require('scheatkode.highlight')
    
     local function is_invalid_buffer()
    -   return
    -             vim.bo.filetype == ''
    -      or     vim.bo.filetype == 'norg'
    -      or     vim.bo.buftype  ~= ''
    -      or not vim.bo.modifiable
    +	return vim.bo.filetype == ''
    +		 or vim.bo.filetype == 'norg'
    +		 or vim.bo.buftype ~= ''
    +		 or not vim.bo.modifiable
     end
    
     local function toggle_trailing(mode)
    -   assert(mode == 'n' or mode == 'i', 'Invalid mode: ' .. mode)
    +	assert(mode == 'n' or mode == 'i', 'Invalid mode: ' .. mode)
    
    -   if is_invalid_buffer() or vim.fn.win_gettype() == 'popup' then
    -      vim.wo.list = false
    -      return
    -   end
    +	if is_invalid_buffer() or vim.fn.win_gettype() == 'popup' then
    +		vim.wo.list = false
    +		return
    +	end
    
    -   if not vim.wo.list then
    -      vim.wo.list = true
    -   end
    +	if not vim.wo.list then
    +		vim.wo.list = true
    +	end
    
    -   local pattern = mode == 'i'
    -      and [[\s\+\%#\@<!$]]
    -      or  [[\s\+$]]
    +	local pattern = mode == 'i'
    +		 and [[\s\+\%#\@<!$]]
    +		 or [[\s\+$]]
    
    -   if vim.w.whitespace_match_number then
    -      vim.fn.matchdelete(vim.w.whitespace_match_number)
    -      vim.fn.matchadd('ExtraWhitespace', pattern, 10, vim.w.whitespace_match_number)
    +	if vim.w.whitespace_match_number then
    +		vim.fn.matchdelete(vim.w.whitespace_match_number)
    +		vim.fn.matchadd('ExtraWhitespace', pattern, 10, vim.w.whitespace_match_number)
    
    -      return
    -   end
    +		return
    +	end
    
    -   vim.w.whitespace_match_number = vim.fn.matchadd('ExtraWhitespace', pattern)
    +	vim.w.whitespace_match_number = vim.fn.matchadd('ExtraWhitespace', pattern)
     end
    
    -h.set_hl('ExtraWhitespace', { guifg = 'red' })
    +vim.api.nvim_set_hl(0, 'ExtraWhitespace', { foreground = 'red' })
    
    -local augroup = vim.api.nvim_create_augroup('WhitespaceHighlight', {clear = true})
    +local augroup = vim.api.nvim_create_augroup('WhitespaceHighlight', { clear = true })
    
     vim.api.nvim_create_autocmd('ColorScheme', {
     	group    = augroup,
    -	callback = function () h.set_hl('ExtraWhitespace', {guifg = 'red'}) end,
    +	callback = function()
    +		vim.api.nvim_set_hl(0, 'ExtraWhitespace', { foreground = 'red' })
    +	end,
     })
    
    -vim.api.nvim_create_autocmd({'BufEnter', 'FileType', 'InsertLeave'}, {
    +vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType', 'InsertLeave' }, {
     	group    = augroup,
    -	callback = function () toggle_trailing('n') end,
    +	callback = function() toggle_trailing('n') end,
     })
    
     vim.api.nvim_create_autocmd('InsertEnter', {
     	group    = augroup,
    -	callback = function () toggle_trailing('i') end,
    +	callback = function() toggle_trailing('i') end,
     })

--- xdg/neovim/lua/scheatkode/whitespace.lua
+++ xdg/neovim/lua/scheatkode/whitespace.lua
@@ -1,54 +1,55 @@
 local h = require('scheatkode.highlight')
 
 local function is_invalid_buffer()
-   return
-             vim.bo.filetype == ''
-      or     vim.bo.filetype == 'norg'
-      or     vim.bo.buftype  ~= ''
-      or not vim.bo.modifiable
+	return vim.bo.filetype == ''
+		 or vim.bo.filetype == 'norg'
+		 or vim.bo.buftype ~= ''
+		 or not vim.bo.modifiable
 end
 
 local function toggle_trailing(mode)
-   assert(mode == 'n' or mode == 'i', 'Invalid mode: ' .. mode)
+	assert(mode == 'n' or mode == 'i', 'Invalid mode: ' .. mode)
 
-   if is_invalid_buffer() or vim.fn.win_gettype() == 'popup' then
-      vim.wo.list = false
-      return
-   end
+	if is_invalid_buffer() or vim.fn.win_gettype() == 'popup' then
+		vim.wo.list = false
+		return
+	end
 
-   if not vim.wo.list then
-      vim.wo.list = true
-   end
+	if not vim.wo.list then
+		vim.wo.list = true
+	end
 
-   local pattern = mode == 'i'
-      and [[\s\+\%#\@<!$]]
-      or  [[\s\+$]]
+	local pattern = mode == 'i'
+		 and [[\s\+\%#\@<!$]]
+		 or [[\s\+$]]
 
-   if vim.w.whitespace_match_number then
-      vim.fn.matchdelete(vim.w.whitespace_match_number)
-      vim.fn.matchadd('ExtraWhitespace', pattern, 10, vim.w.whitespace_match_number)
+	if vim.w.whitespace_match_number then
+		vim.fn.matchdelete(vim.w.whitespace_match_number)
+		vim.fn.matchadd('ExtraWhitespace', pattern, 10, vim.w.whitespace_match_number)
 
-      return
-   end
+		return
+	end
 
-   vim.w.whitespace_match_number = vim.fn.matchadd('ExtraWhitespace', pattern)
+	vim.w.whitespace_match_number = vim.fn.matchadd('ExtraWhitespace', pattern)
 end
 
-h.set_hl('ExtraWhitespace', { guifg = 'red' })
+vim.api.nvim_set_hl(0, 'ExtraWhitespace', { foreground = 'red' })
 
-local augroup = vim.api.nvim_create_augroup('WhitespaceHighlight', {clear = true})
+local augroup = vim.api.nvim_create_augroup('WhitespaceHighlight', { clear = true })
 
 vim.api.nvim_create_autocmd('ColorScheme', {
 	group    = augroup,
-	callback = function () h.set_hl('ExtraWhitespace', {guifg = 'red'}) end,
+	callback = function()
+		vim.api.nvim_set_hl(0, 'ExtraWhitespace', { foreground = 'red' })
+	end,
 })
 
-vim.api.nvim_create_autocmd({'BufEnter', 'FileType', 'InsertLeave'}, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType', 'InsertLeave' }, {
 	group    = augroup,
-	callback = function () toggle_trailing('n') end,
+	callback = function() toggle_trailing('n') end,
 })
 
 vim.api.nvim_create_autocmd('InsertEnter', {
 	group    = augroup,
-	callback = function () toggle_trailing('i') end,
+	callback = function() toggle_trailing('i') end,
 })
