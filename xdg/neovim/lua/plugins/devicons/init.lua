bb7340d343ee245a69a460a63567586a96e33163
scheatkode <scheatkode@gmail.com>
2022-09-30 22:07:32 +0100

    style(neovim/p): Format code
    diff --git a/xdg/neovim/lua/plugins/devicons/init.lua b/xdg/neovim/lua/plugins/devicons/init.lua
    index 03eb05c..942b3c8 100644
    --- a/xdg/neovim/lua/plugins/devicons/init.lua
    +++ b/xdg/neovim/lua/plugins/devicons/init.lua
    @@ -1,5 +1,9 @@
    -return { 'kyazdani42/nvim-web-devicons', opt = true,
    +return {
    +	'kyazdani42/nvim-web-devicons',
    +
    +	opt    = true,
     	module = 'nvim-web-devicons',
    +
     	config = function()
     		require('nvim-web-devicons').setup({
     			default = true,

--- xdg/neovim/lua/plugins/devicons/init.lua
+++ xdg/neovim/lua/plugins/devicons/init.lua
@@ -1,5 +1,9 @@
-return { 'kyazdani42/nvim-web-devicons', opt = true,
+return {
+	'kyazdani42/nvim-web-devicons',
+
+	opt    = true,
 	module = 'nvim-web-devicons',
+
 	config = function()
 		require('nvim-web-devicons').setup({
 			default = true,
