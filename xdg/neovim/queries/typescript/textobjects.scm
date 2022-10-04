a614291bc3aff8b18376e293429e6fc055939158
scheatkode <scheatkode@gmail.com>
2022-10-04 20:07:29 +0100

    feat(neovim/q): Add `typescript` queries from `javascript`
    diff --git a/xdg/neovim/queries/typescript/textobjects.scm b/xdg/neovim/queries/typescript/textobjects.scm
    index e57228b..c9c2cbf 100644
    --- a/xdg/neovim/queries/typescript/textobjects.scm
    +++ b/xdg/neovim/queries/typescript/textobjects.scm
    @@ -1,3 +1,51 @@
    +;; formal parameters in function declarations
    +;;
    +;; ```typescript
    +;; const func = (x: number,  y = 0) => 2 * x + y
    +;; //            ╰───────╯   ╰───╯
    +;; //              ╰─ captures ─╯
    +;; ```
    +(formal_parameters (_) @swappable)
    +
    +;; arguments in function call expressions
    +;;
    +;; ```typescript
    +;; func(Math.PI,  Math.E)
    +;; //   ╰─────╯   ╰────╯
    +;; //    ╰─ captures ─╯
    +;; ```
    +(arguments (_) @swappable)
    +
    +;; branches of ternary expressions
    +;;
    +;; ```typescript
    +;; true ? do_something() : do_nothing()
    +;; //     ╰────────────╯   ╰──────────╯
    +;; //            ╰─ captures ─╯
    +;; ```
    +(ternary_expression [consequence: (_) alternative: (_)] @swappable)
    +
    +;; Properties of objects.
    +;;
    +;; ```typescript
    +;; const obj = { property1: 1, property2: 2 }
    +;; //            ╰──────────╯  ╰──────────╯
    +;; //                 ╰─ captures ─╯
    +;; func({ property1, property2 })
    +;; //     ╰───────╯  ╰───────╯
    +;; //        ╰─ captures ─╯
    +;; ```
    +(object (pair) @swappable)
    +
    +;; Items of arrays.
    +;;
    +;; ```typescript
    +;; const arr = [ member1, member2 ]
    +;; //            ╰─────╯  ╰─────╯
    +;; //             ╰─ captures ─╯
    +;; ```
    +(array (_) @swappable)
    +
     ;; Identifiers in named imports.
     ;;
     ;; ```typescript

--- xdg/neovim/queries/typescript/textobjects.scm
+++ xdg/neovim/queries/typescript/textobjects.scm
@@ -1,3 +1,51 @@
+;; formal parameters in function declarations
+;;
+;; ```typescript
+;; const func = (x: number,  y = 0) => 2 * x + y
+;; //            ╰───────╯   ╰───╯
+;; //              ╰─ captures ─╯
+;; ```
+(formal_parameters (_) @swappable)
+
+;; arguments in function call expressions
+;;
+;; ```typescript
+;; func(Math.PI,  Math.E)
+;; //   ╰─────╯   ╰────╯
+;; //    ╰─ captures ─╯
+;; ```
+(arguments (_) @swappable)
+
+;; branches of ternary expressions
+;;
+;; ```typescript
+;; true ? do_something() : do_nothing()
+;; //     ╰────────────╯   ╰──────────╯
+;; //            ╰─ captures ─╯
+;; ```
+(ternary_expression [consequence: (_) alternative: (_)] @swappable)
+
+;; Properties of objects.
+;;
+;; ```typescript
+;; const obj = { property1: 1, property2: 2 }
+;; //            ╰──────────╯  ╰──────────╯
+;; //                 ╰─ captures ─╯
+;; func({ property1, property2 })
+;; //     ╰───────╯  ╰───────╯
+;; //        ╰─ captures ─╯
+;; ```
+(object (pair) @swappable)
+
+;; Items of arrays.
+;;
+;; ```typescript
+;; const arr = [ member1, member2 ]
+;; //            ╰─────╯  ╰─────╯
+;; //             ╰─ captures ─╯
+;; ```
+(array (_) @swappable)
+
 ;; Identifiers in named imports.
 ;;
 ;; ```typescript
