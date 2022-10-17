d75e34fa42be8f3294f3a6672fc4484b5012fe12
scheatkode <scheatkode@gmail.com>
2022-10-17 15:23:13 +0100

    refactor(neovim/lsp): Refactor lsp handlers setup

--- xdg/neovim/lua/lang/handlers.lua
+++ xdg/neovim/lua/lang/handlers.lua
@@ -1,26 +1,26 @@
-local f = require('f')
-local m = {}
+return {
+	setup = function(options)
+		local f      = require('f')
+		local tablex = require('tablex')
 
-local defaults = {
-   ['textDocument/publishDiagnostics'] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-         virtual_text = {
-            prefix  = '»',
-            spacing = 3,
-         },
+		local defaults = {
+			['textDocument/publishDiagnostics'] = vim.lsp.with(
+				vim.lsp.diagnostic.on_publish_diagnostics, {
+				virtual_text = {
+				   prefix  = '»',
+				   spacing = 3,
+				},
 
-         underline = true,
+				underline = true,
 
-         signs = true,
-         update_in_insert = false,
-      }
-   )
+				signs = true,
+				update_in_insert = false,
+			}
+			)
+		}
+
+		f
+			 .iterate(tablex.deep_extend('force', defaults, options or {}))
+			 :foreach(function( k, v) vim.lsp.handlers[k] = v end)
+	end
 }
-
---- TODO(scheatkode): documentation
-function m.setup(options)
-   f.iterate(vim.tbl_extend('force', defaults, options or {}))
-    :foreach(function (k, v) vim.lsp.handlers[k] = v end)
-end
-
-return m
