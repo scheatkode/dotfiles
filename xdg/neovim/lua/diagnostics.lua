cc6674d5af6417085a60d2518d6c6f784533e25b
scheatkode <scheatkode@gmail.com>
2022-10-17 15:16:01 +0100

    refactor(neovim/u): Refactor diagnostics setup

--- xdg/neovim/lua/diagnostics.lua
+++ xdg/neovim/lua/diagnostics.lua
@@ -1,37 +1,34 @@
-local vfun   = vim.fn
-local tablex = require('tablex')
-
-local defaults = {
-   DiagnosticSignError = { text = '', texthl = 'DiagnosticError' },
-   DiagnosticSignWarn  = { text = '‼', texthl = 'DiagnosticWarn'  },
-   DiagnosticSignInfo  = { text = 'ℹ', texthl = 'DiagnosticInfo'  },
-   DiagnosticSignHint  = { text = '', texthl = 'DiagnosticHint'  },
-}
-
-vim.diagnostic.config({
-   signs            = true,
-   underline        = false,
-   update_in_insert = false,
-   virtual_text     = {
-      spacing = 3,
-   },
-   float = {
-   	source = 'always',
-   },
-})
-
----apply the given diagnostic signs configuration, or the
----defaults otherwise.
----
----@param overrides? table options table
-local function setup (overrides)
-   local options = tablex.deep_extend('force', defaults, overrides or {})
-
-   for k, v in pairs(options) do
-      vfun.sign_define(k, v)
-   end
-end
-
 return {
-   setup = setup
+	---Apply the given diagnostic signs configuration, or the
+	---defaults otherwise.
+	---
+	---@param overrides? table options table
+	setup = function(overrides)
+		vim.diagnostic.config({
+			signs            = true,
+			underline        = false,
+			update_in_insert = false,
+			virtual_text     = {
+				spacing = 3,
+			},
+			float            = {
+				source = 'always',
+			},
+		})
+
+		local tablex = require('tablex')
+
+		local defaults = {
+			DiagnosticSignError = { text = '', texthl = 'DiagnosticError' },
+			DiagnosticSignWarn  = { text = '‼', texthl = 'DiagnosticWarn' },
+			DiagnosticSignInfo  = { text = 'ℹ', texthl = 'DiagnosticInfo' },
+			DiagnosticSignHint  = { text = '', texthl = 'DiagnosticHint' },
+		}
+
+		local options = tablex.deep_extend('force', defaults, overrides or {})
+
+		for k, v in pairs(options) do
+			vim.fn.sign_define(k, v)
+		end
+	end
 }
