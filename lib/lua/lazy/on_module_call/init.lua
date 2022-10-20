537bcc2b10db01e7512c795cecfd24265b7cf015
scheatkode <scheatkode@gmail.com>
2022-10-20 16:44:27 +0100

    fix(lib/lazy): Fix lazy module call
    Use a function instead of a metatable to avoid falsy type-checks as well
    as the added performance and negligible win in memory cost.

--- lib/lua/lazy/on_module_call/init.lua
+++ lib/lua/lazy/on_module_call/init.lua
@@ -2,9 +2,5 @@ local require = require
 
 ---Requires the module at `path` only when it's called.
 return function(path)
-	return setmetatable({}, {
-		__call = function(_, ...)
-			return require(path)(...)
-		end,
-	})
+	return function(...) return require(path)(...) end
 end
