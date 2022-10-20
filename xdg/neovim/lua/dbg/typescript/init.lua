9d2260bf7292e734b7377ec48b9942b692a1e74c
scheatkode <scheatkode@gmail.com>
2022-10-20 19:05:12 +0100

    chore(neovim/dbg): Configure typescript debugging

--- xdg/neovim/lua/dbg/typescript/init.lua
+++ xdg/neovim/lua/dbg/typescript/init.lua
@@ -23,16 +23,14 @@ return {
 			port              = 9229,
 			skipFiles         = { '<node_internals>/**', 'node_modules/**' },
 		}, {
-			name              = 'Vitest (Node2 with ts-node)',
-			type              = 'node2',
-			request           = 'launch',
-			cwd               = vim.loop.cwd(),
-			runtimeArgs       = { '--inspect-brk', '${workspaceFolder}/node_modules/.bin/vitest' },
-			runtimeExecutable = 'node',
-			args              = { '${file}', '--runInBand', '--coverage', 'false' },
-			sourceMaps        = true,
-			port              = 9229,
-			skipFiles         = { '<node_internals>/**', 'node_modules/**' },
+			name                     = 'Attach to running remote process',
+			type                     = 'pwa-node',
+			request                  = 'attach',
+			cwd                      = '${workspaceFolder}',
+			port                     = 9229,
+			skipFiles                = { '<node_internals>/**', 'node_modules/**' },
+			autoAttachChildProcesses = true,
+			sourceMaps               = true,
 		}
 	}
 }
