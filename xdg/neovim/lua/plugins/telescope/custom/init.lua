3c3138d8804534cf3a77f0afab35ee898ecb2e71
scheatkode <scheatkode@gmail.com>
2022-10-20 17:29:19 +0100

    feat(neovim/p): Add custom actions

---
+++ xdg/neovim/lua/plugins/telescope/custom/init.lua
@@ -0,0 +1,5 @@
+local lazy = require('lazy.on_module_call')
+
+return {
+	select_directory = lazy('plugins.telescope.custom.select_directory'),
+}
---
+++ xdg/neovim/lua/plugins/telescope/custom/select_directory.lua
@@ -0,0 +1,31 @@
+return function(operation)
+	local telescope    = require('telescope')
+	local actions      = require('telescope.actions')
+	local action_state = require('telescope.actions.state')
+
+	return function()
+		local current_line = action_state.get_current_line()
+
+		telescope.extensions.file_browser.file_browser({
+			depth = false,
+			files = false,
+			attach_mappings = function()
+				actions.select_default:replace(function()
+					local entry_path = action_state.get_selected_entry().Path
+					local directory  = entry_path:is_dir() and entry_path or entry_path:parent()
+
+					local absolute = directory:absolute()
+					local relative = directory:make_relative(vim.fn.getcwd())
+
+					operation({
+						cwd           = absolute,
+						default_text  = current_line,
+						results_title = relative .. "/",
+					})
+				end)
+
+				return true
+			end,
+		})
+	end
+end
