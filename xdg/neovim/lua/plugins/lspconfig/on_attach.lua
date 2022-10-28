2a4a0ea7047cbf8d645b3fb563d436f16be6b0f5
scheatkode <scheatkode@gmail.com>
2022-10-28 01:12:53 +0100

    fix(neovim/p): Remove `documentHighlight` autocmd
    For some reason, the condition fails in python files; with my short
    CursorHold timeout, the insistent error messages get very annoying very
    fast.
    
    I prefer using the quickfix for this functionality instead anyway, this
    was just some eye candy that I can live without.

--- xdg/neovim/lua/plugins/lspconfig/on_attach.lua
+++ xdg/neovim/lua/plugins/lspconfig/on_attach.lua
@@ -99,14 +99,6 @@ return function(client, bufnr, settings)
 
 	-- }}}
 
-	-- autocommands {{{2
-
-	if client.server_capabilities.documentHighlightProvider then
-		local augroup = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
-		vim.api.nvim_create_autocmd('CursorHold', { buffer = 0, group = augroup, callback = vim.lsp.buf.document_highlight })
-		vim.api.nvim_create_autocmd('CursorMoved', { buffer = 0, group = augroup, callback = vim.lsp.buf.clear_references })
-	end
-
 	-- plugins {{{2
 
 	local has_signature, signature = pcall(require, 'lsp_signature')
