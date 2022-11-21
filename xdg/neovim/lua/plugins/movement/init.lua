7bbf77352ad0f05e9832090226f00bdd3828dd5c
scheatkode <scheatkode@gmail.com>
2022-11-21 20:39:30 +0100

    refactor(neovim/p): Set movement mappings explicitely

--- xdg/neovim/lua/plugins/movement/init.lua
+++ xdg/neovim/lua/plugins/movement/init.lua
@@ -12,6 +12,10 @@ return {
 	},
 
 	config = function()
-		require('leap').add_default_mappings()
+		vim.keymap.set({ 'n' }, 's', '<Plug>(leap-forward-to)')
+		vim.keymap.set({ 'n' }, 'S', '<Plug>(leap-backward-to)')
+		vim.keymap.set({ 'n' }, 'gs', '<Plug>(leap-cross-window)')
+		vim.keymap.set({ 'v', 'x' }, 's', '<Plug>(leap-forward-till)')
+		vim.keymap.set({ 'v', 'x' }, 'S', '<Plug>(leap-backward-till)')
 	end
 }
