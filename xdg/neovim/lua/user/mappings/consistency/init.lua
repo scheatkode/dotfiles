18324be66263502bc6ac419aaf186f37604418b6
scheatkode <scheatkode@gmail.com>
2022-12-03 18:18:29 +0100

    feat(neovim/u): Center screen on half page navigation

--- xdg/neovim/lua/user/mappings/consistency/init.lua
+++ xdg/neovim/lua/user/mappings/consistency/init.lua
@@ -8,6 +8,7 @@ return {
 		require('user.mappings.consistency.quickfix').setup()
 		require('user.mappings.consistency.saner-comma').setup()
 		require('user.mappings.consistency.saner-n').setup()
+		require('user.mappings.consistency.saner-halfpage').setup()
 		require('user.mappings.consistency.selection-indent').setup()
 		require('user.mappings.consistency.shell').setup()
 		require('user.mappings.consistency.refresh').setup()
---
+++ xdg/neovim/lua/user/mappings/consistency/saner-halfpage.lua
@@ -0,0 +1,8 @@
+return {
+	---add automatic screen centering when navigating by
+	---half-page.
+	setup = function()
+		vim.keymap.set('n', '<C-u>', '<C-u>zz')
+		vim.keymap.set('n', '<C-d>', '<C-d>zz')
+	end
+}
