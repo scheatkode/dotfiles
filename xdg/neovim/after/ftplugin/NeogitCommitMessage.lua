56ad110047c1633ef0b5ce0858f35ee06a7547e1
scheatkode <scheatkode@gmail.com>
2022-12-03 18:19:27 +0100

    refactor(neovim/after): Cleanup some ftplugins

--- xdg/neovim/after/ftplugin/NeogitCommitMessage.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set({ 'n', 'i' }, '<C-c><C-c>', '<Esc><cmd>wq<CR>', { buffer = true })
-vim.keymap.set({ 'n', 'i' }, '<C-c><C-k>', '<Esc><cmd>q!<CR>', { buffer = true })
--- xdg/neovim/after/ftplugin/qf.lua
+++ xdg/neovim/after/ftplugin/qf.lua
@@ -1,5 +1,4 @@
 vim.opt_local.buflisted  = false
-vim.opt_local.number     = true
 vim.opt_local.signcolumn = 'no'
 vim.opt_local.bufhidden  = 'wipe'
 
