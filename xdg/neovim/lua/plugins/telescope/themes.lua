f4b48479d12487a100fb4ceee7d9ae0f3acdcf45
scheatkode <scheatkode@gmail.com>
2022-12-18 16:22:19 +0100

    neovim/p: Add theme for telescope command

--- xdg/neovim/lua/plugins/telescope/themes.lua
+++ xdg/neovim/lua/plugins/telescope/themes.lua
@@ -51,4 +51,33 @@ function themes.get_better_cursor(opts)
 	return vim.tbl_deep_extend('force', theme_opts, opts)
 end
 
-return themes
+local function get_command(options)
+	local lazy = require('lazy.on_module_call')
+	local deep_extend = lazy('tablex.deep_extend')
+
+	local defaults = {
+		layout_strategy = 'vertical',
+		sorting_strategy = 'descending',
+		results_title = false,
+
+		layout_config = {
+			prompt_position = 'top',
+			preview_cutoff = 1,
+
+			width = function(_, max_columns, _)
+				return math.min(max_columns, 80)
+			end,
+
+			height = function(_, _, max_lines)
+				return math.min(max_lines, 15)
+			end,
+		},
+	}
+
+	return deep_extend('force', defaults, options or {})
+end
+
+return {
+	get_command = get_command,
+	get_vertical = themes.get_vertical,
+}
