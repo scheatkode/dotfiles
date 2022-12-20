003ff7dc52a6e2e16a2c6a9b5a8e5061a1139dbc
scheatkode <scheatkode@gmail.com>
2022-12-20 20:22:12 +0100

    neovim/p: Adapt `nvim-dap-js` for new plugin manager

---
+++ xdg/neovim/lua/plugins/nvim-dap-js/init.lua
@@ -0,0 +1,21 @@
+return {
+	'mxsdev/nvim-dap-vscode-js',
+
+	config = function()
+		require('dap-vscode-js').setup({
+			adapters = {
+				'node-terminal',
+				'pwa-node',
+				'pwa-chrome',
+				'pwa-msedge',
+				'pwa-extensionHost',
+			},
+
+			debugger_path = string.format(
+				'%s/%s',
+				vim.fn.stdpath('data'),
+				'mason/packages/js-debug-adapter'
+			),
+		})
+	end,
+}
