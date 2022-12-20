d73e053a2acfff72994cfbf04d51b5f08eeeb7eb
scheatkode <scheatkode@gmail.com>
2022-12-20 17:02:01 +0100

    neovim/p: Adapt `nvim-colorizer.lua` for new plugin manager

---
+++ xdg/neovim/lua/plugins/nvim-colorizer/init.lua
@@ -0,0 +1,19 @@
+return { -- colorize hex/rgb/hsl values
+
+	'norcalli/nvim-colorizer.lua',
+
+	ft = {
+		'html',
+		'pug',
+
+		'css',
+		'sass',
+		'scss',
+
+		'lua',
+	},
+
+	config = function()
+		require('colorizer').setup()
+	end,
+}
