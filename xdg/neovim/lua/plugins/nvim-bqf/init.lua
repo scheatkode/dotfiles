05acbed4ff118ec8289f4ef6cfd2ed05cae16f7e
scheatkode <scheatkode@gmail.com>
2022-12-20 18:18:04 +0100

    neovim/p: Adapt `nvim-bqf` for new plugin manager

---
+++ xdg/neovim/lua/plugins/nvim-bqf/init.lua
@@ -0,0 +1,15 @@
+return {
+	'kevinhwang91/nvim-bqf',
+
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter',
+	},
+
+	ft = {
+		'qf',
+	},
+
+	config = function()
+		require('plugins.nvim-bqf.config').setup()
+	end,
+}
