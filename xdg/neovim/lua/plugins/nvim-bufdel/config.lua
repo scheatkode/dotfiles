455fa8d2361403df8663b9e420d5ed978a44efea
scheatkode <scheatkode@gmail.com>
2022-12-20 17:01:28 +0100

    neovim/p: Adapt `nvim-bufdel` for new plugin manager

---
+++ xdg/neovim/lua/plugins/nvim-bufdel/config.lua
@@ -0,0 +1,5 @@
+return {
+	setup = function()
+		require('bufdel').setup({ next = 'alternate', quit = false })
+	end,
+}
---
+++ xdg/neovim/lua/plugins/nvim-bufdel/init.lua
@@ -0,0 +1,16 @@
+return { -- delete buffer without messing up layout
+	'ojroques/nvim-bufdel',
+
+	cmd = {
+		'BufDel',
+	},
+	keys = {
+		'<leader>bk',
+		'<leader>bK',
+	},
+
+	config = function()
+		require('plugins.nvim-bufdel.config').setup()
+		require('plugins.nvim-bufdel.keys').setup()
+	end,
+}
