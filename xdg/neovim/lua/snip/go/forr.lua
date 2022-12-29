37b1fcb799b4871017fda26077db23f1d61bdbe3
scheatkode <scheatkode@gmail.com>
2022-12-29 14:30:24 +0100

    neovim/sn: Add Go `for - range` snippet

---
+++ xdg/neovim/lua/snip/go/forr.lua
@@ -0,0 +1,28 @@
+return {
+	setup = function()
+		local snippet = require('luasnip')
+
+		local i = snippet.insert_node
+		local s = snippet.s
+
+		local f = require('luasnip.extras.fmt').fmta
+
+		snippet.add_snippets('go', {
+			s(
+				'forr',
+				f(
+					[[
+			for <index>, <value> := range <collection> {
+				<finish>
+			}]],
+					{
+						index      = i(1, '_'),
+						value      = i(2, 'v'),
+						collection = i(3, 'v'),
+						finish     = i(0),
+					}
+				)
+			),
+		})
+	end,
+}
