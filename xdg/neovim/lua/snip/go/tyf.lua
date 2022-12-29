f84a7610b18441f7ce923038c37b19901f28f7f8
scheatkode <scheatkode@gmail.com>
2022-12-29 14:27:54 +0100

    neovim/sn: Add Go `tyf` snippet for methods

---
+++ xdg/neovim/lua/snip/go/tyf.lua
@@ -0,0 +1,30 @@
+return {
+	setup = function()
+		local snippet = require('luasnip')
+
+		local i = snippet.insert_node
+		local s = snippet.s
+
+		local f = require('luasnip.extras.fmt').fmta
+
+		snippet.add_snippets('go', {
+			s(
+				'tyf',
+				f(
+					[[
+			func (<type>) <name>(<params>) <return> {
+				<finish>
+			}
+					]],
+					{
+						type       = i(1, 't Type'),
+						name       = i(2, 'FuncName'),
+						params     = i(3),
+						['return'] = i(4, 'error'),
+						finish     = i(0),
+					}
+				)
+			),
+		})
+	end,
+}
