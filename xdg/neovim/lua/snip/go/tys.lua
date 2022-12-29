aaf1d23aae353f84e292b4afd89ac71c6fcd35b5
scheatkode <scheatkode@gmail.com>
2022-12-29 14:28:51 +0100

    neovim/sn: Add Go `tys` snippet for structs

---
+++ xdg/neovim/lua/snip/go/tys.lua
@@ -0,0 +1,27 @@
+return {
+	setup = function()
+		local snippet = require('luasnip')
+
+		local i = snippet.insert_node
+		local s = snippet.s
+
+		local f = require('luasnip.extras.fmt').fmta
+
+		snippet.add_snippets('go', {
+			s(
+				'tys',
+				f(
+					[[
+						type <name> struct {
+							<finish>
+						}
+					]],
+					{
+						name   = i(1, 'TypeName'),
+						finish = i(0),
+					}
+				)
+			),
+		})
+	end,
+}
