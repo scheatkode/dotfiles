539147e365fca4f541cf97bcd38b603ea48f25d1
scheatkode <scheatkode@gmail.com>
2023-01-04 15:00:21 +0100

    neovim/p: Add snippet navigation mappings

--- xdg/neovim/lua/plugins/LuaSnip/keys.lua
+++ xdg/neovim/lua/plugins/LuaSnip/keys.lua
@@ -1,18 +1,32 @@
 return {
 	setup = function()
-		local snippet = require('luasnip')
+		local snippets = require('luasnip')
 
 		local function choice()
-			if snippet.choice_active() then
-				snippet.change_choice(1)
+			if snippets.choice_active() then
+				snippets.change_choice(1)
 			end
 		end
 
-		vim.keymap.set({ 'i' }, '<M-CR>', snippet.expand_or_jump, {
+		local function jump(direction)
+			return function()
+				if snippets.in_snippet() and snippets.jumpable(direction) then
+					return snippets.jump(direction)
+				end
+			end
+		end
+
+		vim.keymap.set({ 'i' }, '<M-CR>', snippets.expand_or_jump, {
 			desc = 'Expand current snippet or jump',
 		})
 		vim.keymap.set({ 'i', 's' }, '<M-e>', choice, {
 			desc = 'Toggle a choice node',
+		})
+		vim.keymap.set({ 'i', 's' }, '<C-a>', jump(1), { -- mnemonic for "after"
+			desc = 'Jump to next snippet node',
+		})
+		vim.keymap.set({ 'i', 's' }, '<C-b>', jump(-1), { -- mnemonic for "before"
+			desc = 'Jump to previous snippet node',
 		})
 	end,
 }
