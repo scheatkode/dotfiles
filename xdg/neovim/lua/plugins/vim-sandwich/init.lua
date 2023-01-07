c9fe33714316b8456baa45250a1ef27cad46782b
scheatkode <scheatkode@gmail.com>
2023-01-07 01:50:11 +0100

    neovim/p: Trigger loading `vim-sandwich` in visual mode

--- xdg/neovim/lua/plugins/vim-sandwich/init.lua
+++ xdg/neovim/lua/plugins/vim-sandwich/init.lua
@@ -2,15 +2,13 @@ return {
 	'machakann/vim-sandwich',
 
 	keys = {
-		'<leader>sa',
-		'<leader>ts',
-		'<leader>tsa',
-		'<leader>tsc',
-		'<leader>tsC',
-		'<leader>tsd',
-		'<leader>tsD',
-		'<leader>ts',
-		'<leader>tsa',
+		{ '<leader>sa',  mode = { 'n', 'x' } },
+		{ '<leader>ts',  mode = { 'n', 'x' } },
+		{ '<leader>tsa', mode = { 'n', 'x' } },
+		{ '<leader>tsc', mode = { 'n', 'x' } },
+		{ '<leader>tsC', mode = { 'n', 'x' } },
+		{ '<leader>tsd', mode = { 'n', 'x' } },
+		{ '<leader>tsD', mode = { 'n', 'x' } },
 	},
 
 	init = function()
