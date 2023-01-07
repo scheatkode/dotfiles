365820ffe9b773c344a20099074cdbc5b0e62379
scheatkode <scheatkode@gmail.com>
2023-01-07 20:40:29 +0100

    lib/lua: Add missing `function` exports

--- lib/lua/f/function/init.lua
+++ lib/lua/f/function/init.lua
@@ -1,10 +1,19 @@
+local lazy = require('load.on_module_call')
+
 return {
-	flip     = require('f.function.flip'),
-	identity = require('f.function.identity'),
-
-	compose = require('f.function.compose'),
-	pipe    = require('f.function.pipe'),
-
-	curry   = require('f.function.curry'),
-	partial = require('f.function.partial'),
+	compose    = lazy('f.function.compose'),
+	constant   = lazy('f.function.constant'),
+	curry      = lazy('f.function.curry'),
+	flip       = lazy('f.function.flip'),
+	identity   = lazy('f.function.identity'),
+	memoize    = lazy('f.function.memoize'),
+	negate     = lazy('f.function.negate'),
+	noop       = lazy('f.function.noop'),
+	nooped     = lazy('f.function.nooped'),
+	partial    = lazy('f.function.partial'),
+	pipe       = lazy('f.function.pipe'),
+	predicates = lazy('f.function.predicates'),
+	rpartial   = lazy('f.function.rpartial'),
+	ternary    = lazy('f.function.ternary'),
+	when       = lazy('f.function.when'),
 }
