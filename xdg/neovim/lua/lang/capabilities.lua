21a3b311db8048338e7ca18b64cacf8dbabb1380
scheatkode <scheatkode@gmail.com>
2023-01-07 02:49:41 +0100

    neovim/lsp: Refactor LSP capabilities

--- xdg/neovim/lua/lang/capabilities.lua
+++ xdg/neovim/lua/lang/capabilities.lua
@@ -1,23 +1,36 @@
 return {
 	setup = function()
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-		capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
-		capabilities.textDocument.completion.completionItem.snippetSupport = true
-		capabilities.textDocument.completion.completionItem.preselectSupport = true
-		capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-		capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-		capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-		capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-		capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-		capabilities.textDocument.completion.completionItem.resolveSupport = {
-			properties = {
-				'documentation',
-				'detail',
-				'additionalTextEdits',
+		local wanted = {
+			textDocument = {
+				completion = {
+					completionItem = {
+						commitCharactersSupport = true,
+						deprecatedSupport       = true,
+						documentationFormat     = {
+                     'markdown',
+                     'plaintext',
+                  },
+						insertReplaceSupport = true,
+						labelDetailsSupport  = true,
+						preselectSupport     = true,
+						resolveSupport       = {
+							properties = {
+								'documentation',
+								'detail',
+								'additionalTextEdits',
+							},
+						},
+						snippetSupport = true,
+						tagSupport     = {
+							valueSet = {
+								1,
+							},
+						},
+					},
+				},
 			},
 		}
 
-		return capabilities
-	end
+		return wanted
+	end,
 }
