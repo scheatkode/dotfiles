ea31f940ef9fa3ea405d953d37956ec1cbeba915
scheatkode <scheatkode@gmail.com>
2023-01-07 03:16:08 +0100

    lib/lua: Enhance code readability

--- lib/lua/tablex/deep_extend/init.lua
+++ lib/lua/tablex/deep_extend/init.lua
@@ -3,6 +3,8 @@ local memoize  = require('f.function.memoize')
 local is_empty = require('tablex.is_empty')
 local is_list  = require('tablex.is_list')
 
+local type = type
+
 local function can_merge(v)
 	return type(v) == 'table' and (is_empty(v) or not is_list(v))
 end
@@ -13,23 +15,25 @@ local function deep_extend_impl(is_mergeable, is_not_force, is_error, ...)
 	for i = 1, select('#', ...) do
 		local t = select(i, ...)
 
-		if t then
-			for k, v in pairs(t) do
-				if is_mergeable(v) and is_mergeable(result[k]) then
-					result[k] = deep_extend_impl(
-						is_mergeable,
-						is_not_force,
-						is_error,
-						result[k],
-						v
-					)
-				elseif is_not_force and result[k] ~= nil then
-					if is_error then
-						error('key found in more than one map: ' .. k)
-					end -- else behavior is 'keep'
-				else
-					result[k] = v
-				end
+		if not t then
+			return result
+		end
+
+		for k, v in pairs(t) do
+			if is_mergeable(v) and is_mergeable(result[k]) then
+				result[k] = deep_extend_impl(
+					is_mergeable,
+					is_not_force,
+					is_error,
+					result[k],
+					v
+				)
+			elseif is_not_force and result[k] ~= nil then
+				if is_error then
+					error('key found in more than one map: ' .. k)
+				end -- else behavior is 'keep'
+			else
+				result[k] = v
 			end
 		end
 	end
