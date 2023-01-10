87a3f0d8dda337cf7361c7d269a3eeddcd62b5a7
scheatkode <scheatkode@gmail.com>
2023-01-10 14:45:48 +0100

    neovim/ts: Document Go treesitter captures

--- xdg/neovim/queries/go/textobjects.scm
+++ xdg/neovim/queries/go/textobjects.scm
@@ -1,7 +1,37 @@
+;; elements of struct definition
+;;
+;; ```go
+;; type Struct struct { some_variable int; some_other_variable any }
+;; //                  ╰────────────────╯  ╰─────────────────────╯
+;; //                              ╰── captures ──╯
+;; ```
 (field_declaration_list (field_declaration) @swappable)
-(literal_value          (keyed_element)     @swappable)
-(literal_value          (literal_element)   @swappable)
 
+;; keyed elements of struct literal
+;;
+;; ```go
+;; v := SomeStruct{A: some_variable, B: some_other_variable}
+;; //              ╰──────────────╯  ╰────────────────────╯
+;; //                      ╰── captures ──╯
+;; ```
+(literal_value (keyed_element)   @swappable)
+
+;; un-keyed elements of struct literal
+;;
+;; ```go
+;; v := SomeStruct{some_variable, some_other_variable}
+;; //              ╰───────────╯  ╰─────────────────╯
+;; //                    ╰── captures ──╯
+;; ```
+(literal_value (literal_element) @swappable)
+
+;; parameters of function declarations
+;;
+;; ```go
+;; func do_something(some_variable int, some_other_variable any)
+;; //                ╰───────────────╯  ╰─────────────────────╯
+;; //                          ╰── captures ──╯
+;; ```
 (function_declaration parameters: (parameter_list (parameter_declaration) @swappable))
 (method_declaration   parameters: (parameter_list (parameter_declaration) @swappable))
 
