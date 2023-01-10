69133592dc13922b5ba47ef8cf2b523a11ad5d14
scheatkode <scheatkode@gmail.com>
2023-01-10 14:23:00 +0100

    neovim/ts: Add capture for Go expression lists

--- xdg/neovim/queries/go/textobjects.scm
+++ xdg/neovim/queries/go/textobjects.scm
@@ -4,3 +4,12 @@
 
 (function_declaration parameters: (parameter_list (parameter_declaration) @swappable))
 (method_declaration   parameters: (parameter_list (parameter_declaration) @swappable))
+
+;; members of expression lists
+;;
+;; ```go
+;;    some_variable, err := do_something()
+;; // ╰───────────╯  ╰─╯
+;; //  ╰── captures ──╯
+;; ```
+(expression_list (_) @swappable)
