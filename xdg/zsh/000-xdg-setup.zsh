9df1b6346a5d8cc98bd65a0e389ed7e029818051
scheatkode <scheatkode@gmail.com>
2023-01-14 23:17:30 +0100

    gmailctl: Add `gmailctl` config

--- xdg/zsh/000-xdg-setup.zsh
+++ xdg/zsh/000-xdg-setup.zsh
@@ -11,3 +11,5 @@ export           RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 export npm_config_userconfig="${XDG_CONFIG_HOME}/npm/npmrc"
 export      npm_config_cache="${XDG_CACHE_HOME}/npm"
 export     npm_config_prefix="${XDG_DATA_HOME}/npm"
+
+alias 'gmailf'='gmailctl --config=${XDG_CONFIG_HOME}/gmailctl'
