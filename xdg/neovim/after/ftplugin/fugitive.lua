3ceb5d8b8955e9fc3a826f9fc565447f116b67b4
scheatkode <scheatkode@gmail.com>
2023-01-17 17:47:22 +0100

    neovim/p: Add `vim-fugitive` `--no-verify` keymaps

--- xdg/neovim/after/ftplugin/fugitive.lua
+++ xdg/neovim/after/ftplugin/fugitive.lua
@@ -12,3 +12,13 @@ vim.keymap.set('n', '<leader>gc', '<cmd>vertical G commit<CR>', {
 	buffer = true,
 	desc   = 'Create a new commit.',
 })
+
+vim.keymap.set('n', 'cnc', '<cmd>G commit --no-verify<CR>', {
+	buffer = true,
+	desc   = 'Create a new commit without running git hooks.'
+})
+
+vim.keymap.set('n', 'cne', '<cmd>G commit --amend --no-edit --no-verify<CR>', {
+	buffer = true,
+	desc   = 'Create a new commit without running git hooks.'
+})
