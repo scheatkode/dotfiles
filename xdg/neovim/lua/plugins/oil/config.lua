a7f054a50789c46d54144280b18fe8e7d69d4ee1
scheatkode <scheatkode@gmail.com>
2023-01-18 13:15:59 +0100

    neovim/p: Use `oil.nvim`'s builtin `open_cmdline`

--- xdg/neovim/lua/plugins/oil/config.lua
+++ xdg/neovim/lua/plugins/oil/config.lua
@@ -2,15 +2,6 @@ return {
 	setup = function()
 		local oil = require('oil')
 
-		local function populate_command()
-			local entry   = oil.get_cursor_entry()
-			local command = ':<C-u> ' .. entry.name .. '<Home>'
-			local keys    =
-				vim.api.nvim_replace_termcodes(command, true, true, true)
-
-			vim.api.nvim_feedkeys(keys, 'nt', false)
-		end
-
 		oil.setup({
 			columns = {
 				'permissions',
@@ -30,17 +21,17 @@ return {
 			keymaps = {
 				['g?'] = 'actions.show_help',
 
-				['<CR>'] = 'actions.select',
+				['<CR>']  = 'actions.select',
 				['<Esc>'] = 'actions.close',
 				['<C-c>'] = 'actions.close',
-				['q'] = 'actions.close',
+				['q']     = 'actions.close',
 
 				['<C-v>'] = 'actions.select_vsplit',
 				['<C-s>'] = 'actions.select_split',
 				['<C-p>'] = 'actions.preview',
 				['<C-l>'] = 'actions.refresh',
 
-				['.'] = populate_command,
+				['.'] = 'actions.open_cmdline',
 
 				['-'] = 'actions.parent',
 				['_'] = 'actions.open_cwd',
