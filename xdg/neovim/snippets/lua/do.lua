902f8429218107d2ebe8dfb9569963263e42dd66
scheatkode <scheatkode@gmail.com>
2023-01-21 17:24:59 +0100

    neovim/sn: Add `do` snippet for Lua loops

---
+++ xdg/neovim/snippets/lua/do.lua
@@ -0,0 +1,24 @@
+local snippet = require('luasnip')
+
+local i = snippet.insert_node
+local s = snippet.s
+
+local f = require('luasnip.extras.fmt').fmta
+
+snippet.add_snippets('lua', {
+	s(
+		'do',
+		f(
+			[[
+do
+	<body>
+end
+<finish>
+			]],
+			{
+				body      = i(1, '-- do something'),
+				finish    = i(0),
+			}
+		)
+	),
+})
