9326bd802a4712f34e6d8cae019d9c23d1f8a83c
scheatkode <scheatkode@gmail.com>
2023-01-22 15:16:24 +0100

    neovim/u: Error when trying to quit an unsaved buffer

--- xdg/neovim/lua/user/settings/files/init.lua
+++ xdg/neovim/lua/user/settings/files/init.lua
@@ -5,7 +5,7 @@ return {
 		vim.opt.autoread     = true -- Detect external file modifications
 		vim.opt.autowrite    = false -- Don't auto write buffer when not focused
 		vim.opt.autowriteall = false -- Don't utomatically save files before running commands
-		vim.opt.confirm      = true -- Prompt to save before being destructive
+		vim.opt.confirm      = false -- Error before quitting an unsaved buffer
 
 		vim.opt.bomb        = false -- Don't prepend bom byte, what is this, Winblows ?
 		vim.opt.encoding    = 'UTF-8' -- Utf8 as standard encoding
