99132e8a55a4e41fc2c5ae8d17a00a8df2578c1a
scheatkode <scheatkode@gmail.com>
2023-01-23 23:49:10 +0100

    neovim/p: Tweak `nvim-autopairs` plugin spec

--- xdg/neovim/lua/plugins/nvim-autopairs/init.lua
+++ xdg/neovim/lua/plugins/nvim-autopairs/init.lua
@@ -1,12 +1,9 @@
 return {
 	'windwp/nvim-autopairs',
 
-	event = {
-		'InsertEnter',
-	},
+	event = 'InsertEnter',
 
 	dependencies = {
-		'hrsh7th/nvim-cmp',
 		'nvim-treesitter/nvim-treesitter',
 	},
 
