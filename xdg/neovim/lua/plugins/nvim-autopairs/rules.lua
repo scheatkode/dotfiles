02d808d8aed396bd1ae8b14ed5f082acb803a738
scheatkode <scheatkode@gmail.com>
2023-02-01 17:56:42 +0100

    neovim/p: Remove custom `nvim-autopairs` rules

--- xdg/neovim/lua/plugins/nvim-autopairs/rules.lua
+++ xdg/neovim/lua/plugins/nvim-autopairs/rules.lua
@@ -14,37 +14,6 @@ return {
 			'yaml',
 		})
 
-		-- additional automatic spacing between pairs
-		rules[#rules + 1] = rule(' ', ' '):with_pair(function(options)
-			local pair = options.line:sub(options.col - 1, options.col)
-			return vim.tbl_contains({
-				'[]',
-				'{}',
-				'%%',
-			}, pair)
-		end)
-
-		-- remove spaces between `%` pairs, useful for templates using `{%%}`
-		rules[#rules + 1] = rule('% ', ' %', {
-				'jinja',
-				'sls',
-				'yaml',
-				'sls.yaml',
-			})
-			:with_pair(cond.none())
-			:with_move(function(options)
-				return options.prev_char:match('.%%%') ~= nil
-			end)
-			:use_key('%')
-
-		-- remove spaces between curly brackets
-		rules[#rules + 1] = rule('{ ', ' }')
-			:with_pair(cond.none())
-			:with_move(function(options)
-				return options.prev_char:match('.%}') ~= nil
-			end)
-			:use_key('}')
-
 		-- remove spaces between brackets
 		rules[#rules + 1] = rule('[ ', ' ]')
 			:with_pair(cond.none())
