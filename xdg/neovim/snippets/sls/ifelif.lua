214de9c231d49170fb2aff88ced2a3ac10fd4a71
scheatkode <scheatkode@gmail.com>
2023-03-17 15:30:40 +0100

    neovim/sn: Add `ifelif` snippet for `sls`

---
+++ xdg/neovim/snippets/sls/ifelif.lua
@@ -0,0 +1,28 @@
+local snippet = require('luasnip')
+
+local i = snippet.insert_node
+local s = snippet.s
+
+local f = require('luasnip.extras.fmt').fmta
+
+snippet.add_snippets('sls', {
+	s(
+		'ifelif',
+		f(
+			[[
+{%- if <condition> %}
+<truthy>
+{%- elif <condition2> %}
+<falsy>
+{%- endif -%}<finish>
+			]],
+			{
+				condition  = i(1, 'true'),
+				condition2 = i(1, 'true'),
+				truthy     = i(2, '# do something'),
+				falsy      = i(3, '# do something else'),
+				finish     = i(0),
+			}
+		)
+	),
+})
