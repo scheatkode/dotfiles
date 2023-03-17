84156771ea2b626fefd07037ba3c3e446572fb86
scheatkode <scheatkode@gmail.com>
2023-03-17 15:30:03 +0100

    neovim/sn: Add `if` snippet for `sls`

---
+++ xdg/neovim/snippets/sls/if.lua
@@ -0,0 +1,24 @@
+local snippet = require('luasnip')
+
+local i = snippet.insert_node
+local s = snippet.s
+
+local f = require('luasnip.extras.fmt').fmta
+
+snippet.add_snippets('sls', {
+	s(
+		'if',
+		f(
+			[[
+{%- if <condition> %}
+<body>
+{%- endif -%}<finish>
+			]],
+			{
+				condition = i(1, 'true'),
+				body      = i(2, '# do something'),
+				finish    = i(0),
+			}
+		)
+	),
+})
