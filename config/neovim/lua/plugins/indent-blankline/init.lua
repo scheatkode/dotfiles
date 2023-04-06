81376749182ada281f19e0e4e4f7c2c9a9d83a8b
scheatkode <scheatkode@gmail.com>
2023-04-06 14:07:15 +0100

    neovim/p: Refactor `indent-blankline` config

--- config/neovim/lua/plugins/indent-blankline/init.lua
+++ config/neovim/lua/plugins/indent-blankline/init.lua
@@ -5,7 +5,48 @@ return {
 		"FileType",
 	},
 
-	config = function()
-		require("plugins.indent-blankline.config").setup()
-	end,
+	opts = {
+		-- default configuration
+
+		char = "│",
+		indent_level = 10,
+		viewport_buffer = 80,
+
+		-- treesitter related configuration
+
+		use_treesitter = true,
+		show_current_context = true,
+		show_trailing_blankline_indent = false,
+
+		show_end_of_line = true,
+
+		context_patterns = {
+			"arguments",
+			"array",
+			"block",
+			"class",
+			"for",
+			"function",
+			"if",
+			"method",
+			"object",
+			"table",
+			"while",
+		},
+
+		-- excludes
+
+		buftype_exclude = {
+			"nofile",
+			"terminal",
+		},
+
+		filetype_exclude = {
+			"fugitive",
+			"help",
+			"markdown",
+			"norg",
+			"oil",
+		},
+	},
 }
