08f4bc3679662e018a957ac3b9243f301b5bc01b
scheatkode <scheatkode@gmail.com>
2023-04-06 16:20:38 +0100

    neovim/u: Refactor path-related mappings

--- config/neovim/lua/user/mappings/custom/path.lua
+++ config/neovim/lua/user/mappings/custom/path.lua
@@ -2,11 +2,16 @@ return {
 	---shortcut for the current file's directory, better than
 	---manually typing `%:p:h` every time.
 	setup = function()
-		vim.keymap.set(
-			"c",
-			",,e",
-			'<C-r>=expand("%:p:h") . "/"<CR>',
-			{ nowait = true }
-		)
+		vim.keymap.set("c", "<m-e>", [[<C-r>=fnameescape("")<left><left>]], {
+			desc = [[Put fnameescape("…")]],
+		})
+
+		vim.keymap.set("c", "<m-t>", [[<C-r>=fnamemodify(@%, ":t")<CR>]], {
+			desc = "Put filename tail",
+		})
+
+		vim.keymap.set("c", "<m-f>", [[<C-r>=expand("%:p:h") . "/"<CR>]], {
+			desc = "Put path of folder containing file",
+		})
 	end,
 }
