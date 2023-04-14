1c2c818b74d01f67b7b7d14062e0fbb2802774ba
scheatkode <scheatkode@gmail.com>
2023-04-14 16:39:11 +0100

    awesome: Add binding for client inspection

--- config/awesome/bindings/client/mouse.lua
+++ config/awesome/bindings/client/mouse.lua
@@ -12,6 +12,30 @@ return gears.table.join(
 		awful.mouse.client.move(c)
 	end),
 
+	awful.button({ mod.super }, 2, function(c)
+		local naughty = require("naughty")
+		naughty.notify({
+			preset = naughty.config.presets.low,
+			title = "Client inspection",
+			text = string.format(
+				table.concat({
+					"name: %s",
+					"instance: %s",
+					"role: %s",
+					"class: %s",
+					"type: %s",
+					"modal: %s",
+				}, "\n"),
+				c.name,
+				c.instance,
+				c.role,
+				c.class,
+				c.type,
+				c.modal
+			),
+		})
+	end),
+
 	awful.button({ mod.super }, 3, function(c)
 		c:emit_signal("request::activate", "mouse_click", { raise = true })
 		awful.mouse.client.resize(c)
