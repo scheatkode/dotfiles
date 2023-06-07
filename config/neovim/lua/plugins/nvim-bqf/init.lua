6cd62a9e8338215a715d6c710cadbb922bc38276
scheatkode <scheatkode@proton.me>
2023-06-07 16:44:58 +0100

    neovim/p: Update `bqf` config

--- config/neovim/lua/plugins/nvim-bqf/init.lua
+++ config/neovim/lua/plugins/nvim-bqf/init.lua
@@ -11,17 +11,7 @@ return {
 
 	opts = {
 		preview = {
-			border_chars = {
-				"",
-				"",
-				"",
-				"",
-				"",
-				"",
-				"",
-				"",
-				"",
-			},
+			border = "none",
 		},
 	},
 }
