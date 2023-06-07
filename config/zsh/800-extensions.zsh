3ff98b47675c640b47ab54a797f6960629fd4617
scheatkode <scheatkode@proton.me>
2023-06-07 16:44:26 +0100

    zsh: Add some `fg`/`bg` parkour

---
+++ config/zsh/800-extensions.zsh
@@ -0,0 +1,14 @@
+# shellcheck shell=zsh
+
+fancy-ctrl-z () {
+	if [ "${#BUFFER}" = '0' ]; then
+		BUFFER="fg"
+		zle accept-line
+	else
+		zle push-input
+		zle clear-screen
+	fi
+}
+
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
