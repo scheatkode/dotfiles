9a10b2cafc54de3dc071756022a510c8864cd432
scheatkode <scheatkode@disroot.org>
2023-06-23 23:26:31 +0100

    neovim/u: Use the OG `^[` for escaping

--- config/neovim/lua/user/mappings/custom/escape.lua
+++ config/neovim/lua/user/mappings/custom/escape.lua
@@ -2,8 +2,5 @@ return {
 	---faster escaping
 	setup = function()
 		vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-
-		vim.keymap.set({ "i", "v", "c", "o" }, "jk", "<Esc>")
-		vim.keymap.set({ "i", "v", "c", "o" }, "<C-c>", "<Esc>")
 	end,
 }
