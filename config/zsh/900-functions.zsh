0d7e7a9cb2b9d81c128b4d1c9ae98e2576a9f306
scheatkode <scheatkode@disroot.org>
2023-07-13 15:27:12 +0100

    bin: Extract `drop-caches` to `,memflush` script

--- config/zsh/900-functions.zsh
+++ config/zsh/900-functions.zsh
@@ -1,30 +1,3 @@
-
-function get-uid () { echo "$(id -u)" ; }
-
-function is-root() {
-   if [ x"0" != x"$(get-uid)" ] ; then
-      return 1
-   fi
-
-   return 0
-}
-
-function quit-if-not-root () {
-   if ! is-root ; then
-      echo 'Must be run as root.'
-      kill -INT $$
-   fi
-}
-
-function drop-caches () {
-   if ! is-root ; then
-      sudo sh -c 'swapoff -a && swapon -a && echo 3 > /proc/sys/vm/drop_caches'
-      return 0
-   fi
-
-   swapoff -a && swapon -a && echo 3 > /proc/sys/vm/drop_caches
-}
-
 function ascii-art () {
    echo "${@}" | awk '
 BEGIN {
