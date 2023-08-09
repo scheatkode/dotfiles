4e84a71eebe457f7b9b2e4faf55cc474f0861d34
scheatkode <scheatkode@disroot.org>
2023-08-09 17:09:00 +0100

    neovim/lsp: Rework `lua-ls` configuration

--- config/neovim/lua/lsp/servers/lua_ls.lua
+++ config/neovim/lua/lsp/servers/lua_ls.lua
@@ -1,73 +1,86 @@
-local fn = vim.fn
-
-local has_lspconfig, lspconfig = pcall(require, "lspconfig")
-
-if not has_lspconfig then
-	print(
-		"‼ Tried loading lspconfig for lua-language-server ... unsuccessfully."
-	)
-	return has_lspconfig
-end
-
-local runtime_path = vim.split(package.path, ";")
-
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
--- TODO(scheatkode): Add autoinstall with spinner animation
-
 return {
 	settings = {
-
 		Lua = {
 			telemetry = {
 				enable = false,
 			},
 
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most
-				-- likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
-			},
-
 			diagnostics = {
 				enable = true,
-				-- Get the language server to recognize the `vim` global
-				globals = {
-					-- Neovim specific
-					"vim",
-					"use", -- Packer `use` keyword
-
-					-- Busted specific
-					"describe",
-					"it",
-					"before_each",
-					"after_each",
-
-					-- AwesomeWM specific
-					"awesome",
-					"client",
-					"root",
-					"screen",
-				},
 			},
 
 			hint = {
 				enable = true,
-			},
-
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				-- library = vim.api.nvim_get_runtime_file('', true),
-				library = {
-					[fn.expand("$VIMRUNTIME/lua")] = true,
-					-- [fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-					[fn.expand("$VIMRUNTIME/lua/lsp")] = true,
-					[fn.expand("/usr/share/awesome/lib")] = true,
-				},
+				paramName = "Literal",
+				setType = true,
 			},
 		},
 	},
+
+	on_init = function(client)
+		local compat = require("compat")
+		local path = client.workspace_folders[1].name
+
+		if
+			vim.loop.fs_stat(
+				string.format("%s%s.luarc.json", path, compat.path_separator)
+			)
+			or vim.loop.fs_stat(
+				string.format("%s%s.luarc.jsonc", path, compat.path_separator)
+			)
+		then
+			return
+		end
+
+		local sx = require("stringx")
+		local tx = require("tablex")
+
+		local constant = require("f.function.constant")
+		local ternary = require("f.function.ternary")
+
+		local in_dotfiles = sx.endswith(path, "/scheatkode/dotfiles")
+		local settings = tx.deep_extend("force", client.config.settings, {
+			Lua = {
+				diagnostics = {
+					enable = true,
+					-- Get the language server to recognize some globals.
+					globals = ternary(
+						in_dotfiles,
+						constant({
+							-- Neovim specific
+							"vim",
+
+							-- Busted specific
+							"describe",
+							"it",
+							"before_each",
+							"after_each",
+
+							-- AwesomeWM specific
+							"awesome",
+							"client",
+							"root",
+							"screen",
+						}),
+						constant(nil)
+					),
+				},
+
+				runtime = {
+					version = "LuaJIT",
+				},
+
+				workspace = ternary(
+					in_dotfiles,
+					constant({ library = { vim.env.VIMRUNTIME } }),
+					constant(nil)
+				),
+			},
+		})
+
+		client.notify(
+			"workspace/didChangeConfiguration",
+			{ settings = settings }
+		)
+	end,
 }
