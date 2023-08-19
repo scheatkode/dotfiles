90adebc22ba8b2d6a808b64c8d445301b83ba672
scheatkode <scheatkode@disroot.org>
2023-08-19 15:29:51 +0100

    zsh: Export dark mode overrides

--- config/zsh/envvar.plugin.sh
+++ config/zsh/envvar.plugin.sh
@@ -13,6 +13,11 @@ if command -v firefox > /dev/null 2>&1; then
    export BROWSER='firefox'
 fi
 
+# Use dark mode
+
+export GTK_THEME=Adwaita:dark
+export QT_STYLE_OVERRIDE=adwaita-dark
+
 # disable branding & telemetry
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=yes
