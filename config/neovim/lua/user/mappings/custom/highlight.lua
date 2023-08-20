ca20f06dfaf2f464352c094b33db5c312726a667
scheatkode <scheatkode@disroot.org>
2023-08-20 13:11:01 +0100

    neovim/u: Add `desc` field to jumplist mappings

--- config/neovim/lua/user/mappings/custom/highlight.lua
+++ config/neovim/lua/user/mappings/custom/highlight.lua
@@ -1,11 +1,9 @@
 return {
 	---remove search highlight on `<Esc>`
 	setup = function()
-		vim.keymap.set(
-			"n",
-			"<Esc>",
-			"<cmd>:nohlsearch<CR>",
-			{ remap = true, silent = true }
-		)
+		vim.keymap.set("n", "<Esc>", "<cmd>:nohlsearch<CR>", {
+			remap = true,
+			silent = true,
+		})
 	end,
 }
--- config/neovim/lua/user/mappings/custom/jumplist.lua
+++ config/neovim/lua/user/mappings/custom/jumplist.lua
@@ -2,7 +2,13 @@ return {
 	---workaround for going up/down the jump list since most terminal
 	---multiplexers don't support `CSI u`.
 	setup = function()
-		vim.keymap.set("n", "]j", "<C-i>", { nowait = true })
-		vim.keymap.set("n", "[j", "<C-o>", { nowait = true })
+		vim.keymap.set("n", "]j", "<C-i>", {
+			desc = "Go to [count] older cursor position in jump list",
+			nowait = true,
+		})
+		vim.keymap.set("n", "[j", "<C-o>", {
+			desc = "Go to [count] newer cursor position in jump list",
+			nowait = true,
+		})
 	end,
 }
