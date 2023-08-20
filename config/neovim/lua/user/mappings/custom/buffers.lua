aaa32f4721c374f0dbeb179f9624d936e1855572
scheatkode <scheatkode@disroot.org>
2023-08-20 13:08:38 +0100

    neovim/u: Make buffer mappings consistent with tabs

--- config/neovim/lua/user/mappings/custom/buffers.lua
+++ config/neovim/lua/user/mappings/custom/buffers.lua
@@ -1,35 +1,26 @@
 return {
 	---buffer operations
 	setup = function()
-		vim.keymap.set(
-			"n",
-			"<leader>bn",
-			"<cmd>enew<CR>",
-			{ desc = "New empty buffer" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>bw",
-			"<cmd>bwipeout!<CR>",
-			{ desc = "Wipe buffer" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>bq",
-			"<cmd>bunload<CR>",
-			{ desc = "Quit/Close buffer" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>br",
-			"<cmd>e<CR>",
-			{ desc = "Reload buffer" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>bR",
-			"<cmd>e!<CR>",
-			{ desc = "Reload buffer forcefully" }
-		)
+		vim.keymap.set("n", "<leader>bN", "<cmd>enew<CR>", {
+			desc = "New empty buffer",
+		})
+		vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", {
+			desc = "Next buffer",
+		})
+		vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", {
+			desc = "Previous buffer",
+		})
+		vim.keymap.set("n", "<leader>bw", "<cmd>bwipeout!<CR>", {
+			desc = "Wipe buffer",
+		})
+		vim.keymap.set("n", "<leader>bq", "<cmd>bunload<CR>", {
+			desc = "Quit/Close buffer",
+		})
+		vim.keymap.set("n", "<leader>br", "<cmd>e<CR>", {
+			desc = "Reload buffer",
+		})
+		vim.keymap.set("n", "<leader>bR", "<cmd>e!<CR>", {
+			desc = "Reload buffer forcefully",
+		})
 	end,
 }
