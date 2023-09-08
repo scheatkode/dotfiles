ccbd0ab2c91746e9a7efdae928c3f13e7032a053
scheatkode <scheatkode@disroot.org>
2023-09-08 18:53:22 +0100

    neovim/ft: Load `Cfilter` when in quickfix

--- config/neovim/after/ftplugin/qf.lua
+++ config/neovim/after/ftplugin/qf.lua
@@ -2,6 +2,8 @@ vim.opt_local.buflisted = false
 vim.opt_local.signcolumn = "no"
 vim.opt_local.bufhidden = "wipe"
 
+vim.cmd.packadd("cfilter")
+vim.cmd.cnoreabbrev({ "cfilter", "Cfilter" })
 vim.cmd([[autocmd! BufEnter <buffer> if winnr('$') < 2 | q | endif]])
 
 vim.keymap.set("n", "q", "<cmd>q<CR>", {
