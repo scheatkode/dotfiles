299b64e4902ad5b54a94232115605db3ddf6f30b
scheatkode <scheatkode@disroot.org>
2023-10-01 13:10:14 +0100

    neovim/p: Update schemastore config
    Signed-Off-By: scheatkode <scheatkode@disroot.org>

--- config/neovim/lua/plugins/SchemaStore/init.lua
+++ config/neovim/lua/plugins/SchemaStore/init.lua
@@ -6,4 +6,39 @@ return { -- Access to the SchemaStore catalog.
 		"json",
 		"yaml",
 	},
+
+	dependencies = {
+		"neovim/nvim-lspconfig",
+	},
+
+	config = function()
+		local schemastore = require("schemastore")
+
+		require("lspconfig").jsonls.setup({
+			settings = {
+				json = {
+					schemas = schemastore.json.schemas(),
+					valibate = {
+						enable = true,
+					},
+				},
+			},
+		})
+
+		require("lspconfig").yamlls.setup({
+			settings = {
+				yaml = {
+					customTags = {
+						-- GitLab CI specific
+						"!reference sequence",
+					},
+					schemaStore = {
+						enable = false,
+						url = "",
+					},
+					schemas = schemastore.yaml.schemas(),
+				},
+			},
+		})
+	end,
 }
