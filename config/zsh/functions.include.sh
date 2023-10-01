42d6ef0b3e6975f6a3ce486da5cef416b959db40
scheatkode <scheatkode@disroot.org>
2023-10-01 12:59:47 +0100

    zsh: Update utility functions comments
    Signed-Off-By: scheatkode <scheatkode@disroot.org>

--- config/zsh/functions.include.sh
+++ config/zsh/functions.include.sh
@@ -5,41 +5,44 @@
 #
 # shellcheck shell=sh
 
-# @description Nullify a function's outputs.
+# Nullify a function's outputs.
 _dot_void() {
 	"${@}" >/dev/null 2>&1
 }
 
-# @description Nullify a function's error output.
+# Nullify a function's error output.
 _dot_void_err() {
 	"${@}" 2>/dev/null
 }
 
-# @description Nullify a function's standard output.
+# Nullify a function's standard output.
 _dot_void_out() {
 	"${@}" >/dev/null
 }
 
-# @description Strip from the left of the given string.
-# @arg The string to strip from.
-# @arg The string to strip.
-# @returns The stripped string.
+# Strip from the left of the given string.
+#
+# :param 1: The string to strip from.
+# :param 2: The string to strip.
+# :returns: The stripped string.
 _dot_lstrip() {
 	printf '%s\n' "${1##"${2}"}"
 }
 
-# @description Strip from the right of the given string.
-# @arg The string to strip from.
-# @arg The string to strip.
-# @returns The stripped string.
+# Strip from the right of the given string.
+#
+# :param 1: The string to strip from.
+# :param 2: The string to strip.
+# :returns: The stripped string.
 _dot_rstrip() {
 	printf '%s\n' "${1%%"${2}"}"
 }
 
-# @description Split the given string.
-# @arg The string to split.
-# @arg The characters to split the string with.
-# @returns The split string.
+# Split the given string.
+#
+# :param 1: The string to split.
+# :param 2: The characters to split the string with.
+# :returns: The split string.
 _dot_split() {
 	set -f
 	__ye_olde_ifs=${IFS}
