d7749d10c8381162cabbd27b7e2eb122e2c878c0
scheatkode <scheatkode@disroot.org>
2023-11-05 12:29:45 +0100

    zsh: Better document included functions
    This makes the function's intent and behaviour clearer.
    
    Signed-Off-By: scheatkode <scheatkode@disroot.org>

--- config/zsh/functions.include.sh
+++ config/zsh/functions.include.sh
@@ -5,44 +5,65 @@
 #
 # shellcheck shell=sh
 
-# Nullify a function's outputs.
+# Nullify a command's outputs.
+#
+# @arg $* (anything) The command to run.
+#
+# @exitcode * The exitcode of the given command.
 _dot_void() {
 	"${@}" >/dev/null 2>&1
 }
 
-# Nullify a function's error output.
+# Nullify a command's error output.
+#
+# @arg $* (anything) The command to run.
+#
+# @exitcode * The exitcode of the given command.
 _dot_void_err() {
 	"${@}" 2>/dev/null
 }
 
-# Nullify a function's standard output.
+# Nullify a command's standard output.
+#
+# @arg $* (anything) The command to run.
+#
+# @exitcode * The exitcode of the given command.
 _dot_void_out() {
 	"${@}" >/dev/null
 }
 
 # Strip from the left of the given string.
 #
-# :param 1: The string to strip from.
-# :param 2: The string to strip.
-# :returns: The stripped string.
+# @arg $1 (string) The string to strip from.
+# @arg $2 (string) The string to strip.
+#
+# @stdout The stripped string.
+#
+# @exitcode 0 Always.
 _dot_lstrip() {
 	printf '%s\n' "${1##"${2}"}"
 }
 
 # Strip from the right of the given string.
 #
-# :param 1: The string to strip from.
-# :param 2: The string to strip.
-# :returns: The stripped string.
+# @arg $1 (string) The string to strip from.
+# @arg $2 (string) The string to strip.
+#
+# @stdout The stripped string.
+#
+# @exitcode 0 Always.
 _dot_rstrip() {
 	printf '%s\n' "${1%%"${2}"}"
 }
 
 # Split the given string.
 #
-# :param 1: The string to split.
-# :param 2: The characters to split the string with.
-# :returns: The split string.
+# @arg $1 (string) The string to split.
+# @arg $2 (string) The characters to split the string with.
+#
+# @stdout The split string.
+#
+# @exitcode 0 Always.
 _dot_split() {
 	set -f
 	__ye_olde_ifs=${IFS}
@@ -57,4 +78,14 @@ _dot_split() {
 	set +f
 
 	unset __ye_olde_ifs
+}
+
+# Check for the availability of a command/function.
+#
+# @arg $1 (string) The command/function name to check for.
+#
+# @exitcode 0 If the command/function exists.
+# @exitcode 1 If the command/function does not exist.
+_dot_has() {
+	_dot_void command -v "${1}"
 }
